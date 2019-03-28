aa2fe8ff0ead0232a0c4a8f1f5e60f9bd9214884
Koichi Shiraishi <zchee.io@gmail.com>
2019-03-28 21:16:33 +0900

    glances: add pandoc dependency

--- Formula/glances.rb
+++ Formula/glances.rb
@@ -6,6 +6,7 @@ class Glances < Formula
   head "https://github.com/nicolargo/glances.git"
 
   depends_on "python"
+  depends_on "pandoc"
 
   resource "psutil" do
     url "https://files.pythonhosted.org/packages/7d/9a/1e93d41708f8ed2b564395edfa3389f0fd6d567597401c2e5e2775118d8b/psutil-5.4.7.tar.gz"
