f62d93d0d0620baa9dc9712bc8c9d0f03fc08f91
Koichi Shiraishi <zchee.io@gmail.com>
2019-05-04 03:34:25 +0900

    kubectx: add custom kubectx formula

---
+++ Formula/kubectx.rb
@@ -0,0 +1,23 @@
+class Kubectx < Formula
+  desc "Tool that can switch between kubectl contexts easily and create aliases"
+  homepage "https://github.com/ahmetb/kubectx"
+  head "https://github.com/ahmetb/kubectx.git"
+
+  bottle :unneeded
+
+  def install
+    bin.install "kubectx", "kubens"
+
+    bash_completion.install "completion/kubectx.bash" => "kubectx"
+    bash_completion.install "completion/kubens.bash" => "kubens"
+    zsh_completion.install "completion/kubectx.zsh" => "_kubectx"
+    zsh_completion.install "completion/kubens.zsh" => "_kubens"
+    fish_completion.install "completion/kubectx.fish"
+    fish_completion.install "completion/kubens.fish"
+  end
+
+  test do
+    assert_match "USAGE:", shell_output("#{bin}/kubectx -h 2>&1")
+    assert_match "USAGE:", shell_output("#{bin}/kubens -h 2>&1")
+  end
+end
