90fbd452d6c6b7fb40863aa301d2477c78bada10
Koichi Shiraishi <zchee.io@gmail.com>
2019-09-05 06:33:55 +0900

    libevent-head: remove doxygen dependency

--- Formula/libevent-head.rb
+++ Formula/libevent-head.rb
@@ -5,24 +5,17 @@ class LibeventHead < Formula
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "doxygen" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "openssl"
 
-  conflicts_with "pincaster",
-    :because => "both install `event_rpcgen.py` binaries"
-
   def install
-    inreplace "Doxyfile", /GENERATE_MAN\s*=\s*NO/, "GENERATE_MAN = YES"
     system "./autogen.sh"
     system "./configure", "--disable-dependency-tracking",
                           "--disable-debug-mode",
                           "--prefix=#{prefix}"
     system "make"
     system "make", "install"
-    system "make", "doxygen"
-    man3.install Dir["doxygen/man/man3/*.3"]
   end
 
   test do
