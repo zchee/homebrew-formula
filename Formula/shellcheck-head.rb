00f07e006c897a16a0d49ef46083842318749c3e
Koichi Shiraishi <zchee.io@gmail.com>
2020-04-10 18:20:49 +0900

    shellcheck-head: add shellcheck-head formula

---
+++ Formula/shellcheck-head.rb
@@ -0,0 +1,28 @@
+class ShellcheckHead < Formula
+  desc "Static analysis and lint tool, for (ba)sh scripts"
+  homepage "https://www.shellcheck.net/"
+  head "https://github.com/koalaman/shellcheck.git"
+
+  depends_on "cabal-install" => :build
+  depends_on "ghc" => :build
+  depends_on "pandoc" => :build
+
+  def install
+    system "cabal", "v2-update"
+    system "cabal", "v2-install", *std_cabal_v2_args
+    system "pandoc", "-s", "-f", "markdown-smart", "-t", "man",
+                     "shellcheck.1.md", "-o", "shellcheck.1"
+    man1.install "shellcheck.1"
+  end
+
+  test do
+    sh = testpath/"test.sh"
+    sh.write <<~EOS
+      for f in $(ls *.wav)
+      do
+        echo "$f"
+      done
+    EOS
+    assert_match "[SC2045]", shell_output("#{bin}/shellcheck -f gcc #{sh}", 1)
+  end
+end
