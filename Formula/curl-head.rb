432895d1976069f0beb27a1002c88c7c3e974610
Koichi Shiraishi <zchee.io@gmail.com>
2020-06-17 04:35:27 +0900

    curl-head: update curl-head formula

--- Formula/curl-head.rb
+++ Formula/curl-head.rb
@@ -1,15 +1,6 @@
 class CurlHead < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.haxx.se/"
-  url "https://curl.haxx.se/download/curl-7.64.0.tar.bz2"
-  sha256 "d573ba1c2d1cf9d8533fadcce480d778417964e8d04ccddcc76e591d544cf2eb"
-
-  bottle do
-    cellar :any
-    sha256 "e5fa214a00a00fa80d76bd05db14e5c176a6be7bedeb86d748f5aa2969344f88" => :mojave
-    sha256 "7349c533e22662c05ef5039696c3e8a7cd8d8a696797040c9c6bf27134662feb" => :high_sierra
-    sha256 "0191dd2c0b129db3fcc10de4e5f61891ebe481a376cec04f8a2a4df5b389e9cb" => :sierra
-  end
 
   head do
     url "https://github.com/curl/curl.git"
@@ -17,47 +8,46 @@ class CurlHead < Formula
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
-    depends_on "libressl" => :build
-
-    depends_on "brotli" => :build
-    depends_on "libidn2" => :build
-    depends_on "libmetalink" => :build
-    depends_on "libressl" => :build
-    depends_on "libssh" => :build
-    depends_on "libssh2" => :build
-    depends_on "nghttp2" => :build
-    depends_on "rtmpdump" => :build
-    depends_on "zlib" => :build
   end
 
   depends_on "pkg-config" => :build
+  depends_on "brotli"
+  depends_on "c-ares"
+  depends_on "libidn"
+  depends_on "libmetalink"
+  depends_on "libressl"
+  depends_on "libssh2"
+  depends_on "nghttp2"
+  depends_on "openldap"
+  depends_on "rtmpdump"
+  depends_on "zlib"
 
   def install
     system "./buildconf" if build.head?
 
     args = %W[
-      --prefix=#{prefix}
       --disable-debug
       --disable-dependency-tracking
       --disable-silent-rules
+      --prefix=#{prefix}
       --enable-optimize
       --enable-ipv6
       --enable-pthreads
       --enable-unix-sockets
       --enable-cookies
-      --with-zlib=#{Formula["zlib"].prefix}
-      --with-brotli=#{Formula["brotli"].prefix}
-      --without-darwinssl
-      --with-ssl=#{Formula["libressl"].prefix}
-      --with-ca-bundle=/usr/local/etc/ssl/certs/ca-bundle.crt
-      --with-ca-path=/usr/local/etc/ssl/certs
-      --with-libmetalink=#{Formula["libmetalink"].prefix}
-      --with-libssh2=#{Formula["libssh2"].prefix}
-      --with-libssh=#{Formula["libssh"].prefix}
-      --with-librtmp=#{Formula["rtmpdump"].prefix}
-      --with-libidn2=#{Formula["libidn2"].prefix}
-      --with-nghttp2=#{Formula["nghttp2"].prefix}
+      --enable-ares=#{Formula["c-ares"].opt_prefix}
+      --with-brotli=#{Formula["brotli"].opt_prefix}
+      --with-ca-bundle=#{Formula["libressl"].pkgetc}/cert.pem
+      --with-ca-path=#{Formula["libressl"].pkgetc}/certs
+      --with-libidn2
+      --with-libmetalink
+      --with-librtmp
+      --with-libssh2
+      --with-nghttp2
+      --with-ssl=#{Formula["libressl"].opt_prefix}
+      --with-zlib=#{Formula["zlib"].opt_prefix}
       --with-zsh-functions-dir=#{prefix}/etc/zsh/site-functions
+      --without-libpsl
     ]
 
     system "./configure", *args
