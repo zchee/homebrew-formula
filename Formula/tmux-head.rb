e02eac5c41bd726c482a1f687fddc27897357210
Koichi Shiraishi <zchee.io@gmail.com>
2020-07-12 07:17:49 +0900

    tmux-head: add tmux-head formula

---
+++ Formula/tmux-head.rb
@@ -0,0 +1,56 @@
+class TmuxHead < Formula
+  desc "Terminal multiplexer"
+  homepage "https://tmux.github.io/"
+
+  head do
+    url "https://github.com/tmux/tmux.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "libevent-head"
+  depends_on "ncurses-head"
+
+  # Old versions of macOS libc disagree with utf8proc character widths.
+  # https://github.com/tmux/tmux/issues/2223
+  depends_on "utf8proc" if MacOS.version >= :high_sierra
+
+  resource "completion" do
+    url "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/f5d53239f765/completions/tmux"
+    sha256 "b5f7bbd78f9790026bbff16fc6e3fe4070d067f58f943e156bd1a8c3c99f6a6f"
+  end
+
+  def install
+    system "sh", "autogen.sh" if build.head?
+
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+    ]
+
+    args << "--enable-utf8proc" if MacOS.version >= :high_sierra
+
+    ENV.append "LDFLAGS", "-lresolv"
+    system "./configure", *args
+
+    system "make", "install"
+
+    pkgshare.install "example_tmux.conf"
+    bash_completion.install resource("completion")
+  end
+
+  def caveats
+    <<~EOS
+      Example configuration has been installed to:
+        #{opt_pkgshare}
+    EOS
+  end
+
+  test do
+    system "#{bin}/tmux", "-V"
+  end
+end
