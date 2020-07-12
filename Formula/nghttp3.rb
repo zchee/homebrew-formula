ac778802b815f3cb7a4e8ba000e71e044be15882
Koichi Shiraishi <zchee.io@gmail.com>
2020-07-12 15:42:43 +0900

    nghttp3: add nghttp3

---
+++ Formula/nghttp3.rb
@@ -0,0 +1,26 @@
+class Nghttp3 < Formula
+  desc "HTTP/3 library written in C"
+  homepage "https://github.com/ngtcp2/nghttp3"
+
+  head do
+    url "https://github.com/ngtcp2/nghttp3.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  def install
+    ENV.cxx11
+
+    args = %W[
+      --prefix=#{prefix}
+      --enable-lib-only
+    ]
+
+    system "autoreconf", "-i" if build.head?
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+end
