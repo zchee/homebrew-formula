7708ec12848113c5b35b2f2d9246ead00c734702
Koichi Shiraishi <zchee.io@gmail.com>
2020-08-01 05:32:37 +0900

    file-formula-head: fix install def

--- Formula/file-formula-head.rb
+++ Formula/file-formula-head.rb
@@ -10,16 +10,28 @@ class FileFormulaHead < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "libmagic" => :build
+  depends_on "zlib" => :build
+  depends_on "xz" => :build
 
   def install
     ENV.prepend "LDFLAGS", "-L#{Formula["libmagic"].opt_lib} -lmagic"
 
-    system "autoreconf", "-ivf" if build.head?
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --enable-static
+      --enable-shared
+    ]
+
+    system "autoreconf", "-ivf"
+
+    inreplace "src/Makefile.in", "file_DEPENDENCIES = libmagic.la", ""
+    inreplace "src/Makefile.in", "file_LDADD = libmagic.la", "file_LDADD = $(LDADD)"
+
+    system "./configure", *args
+
     system "make", "install-exec"
     system "make", "-C", "doc", "install-man1"
-    rm_r lib
   end
 
   test do
