3ee90deb0594e7168d1f5d5673580102602a0ef2
Koichi Shiraishi <zchee.io@gmail.com>
2020-08-26 04:55:54 +0900

    zsh-fast-syntax-highlighting: add zsh-fast-syntax-highlighting formula

---
+++ Formula/zsh-fast-syntax-highlighting.rb
@@ -0,0 +1,21 @@
+class ZshFastSyntaxHighlighting < Formula
+  desc "Fast Zsh Syntax Highlighting"
+  homepage "https://github.com/zdharma/fast-syntax-highlighting"
+  head "https://github.com/zdharma/fast-syntax-highlighting.git"
+
+  def install
+    pkgshare.install Dir["*"]
+  end
+
+  def caveats
+    <<~EOS
+      To activate the syntax highlighting, add the following at the end of your .zshrc:
+        source #{HOMEBREW_PREFIX}/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+    EOS
+  end
+
+  test do
+    assert_match "#{version}\n",
+      shell_output("zsh -c '. #{pkgshare}/fast-syntax-highlighting.plugin.zsh && echo $FAST_HIGHLIGHT_VERSION'")
+  end
+end
