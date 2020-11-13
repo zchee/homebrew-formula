2d75d0b4af33f18bb63991110549b96c25339846
Koichi Shiraishi <zchee.io@gmail.com>
2020-11-14 08:15:58 +0900

    less-devel: upgrade to 563

--- Formula/less-devel.rb
+++ Formula/less-devel.rb
@@ -1,8 +1,9 @@
 class LessDevel < Formula
   desc "Pager program similar to more"
   homepage "http://www.greenwoodsoftware.com/less/index.html"
-  url "http://www.greenwoodsoftware.com/less/less-562.tar.gz"
-  sha256 "eab470c7c928132441541aa49b1352c0fc699c30f762dfaeb3bf88e6f0fd701b"
+  url "http://www.greenwoodsoftware.com/less/less-563.tar.gz"
+  sha256 "ce5b6d2b9fc4442d7a07c93ab128d2dff2ce09a1d4f2d055b95cf28dd0dc9a9a"
+  license "GPL-3.0-or-later"
 
   livecheck do
     url :homepage
@@ -11,11 +12,19 @@ class LessDevel < Formula
 
   bottle :unneeded
 
-  depends_on "pcre"
+  depends_on "pcre2"
   depends_on "ncurses-head"
 
+  if MacOS.version >= :catalina
+    depends_on "gcc" => :build
+    fails_with :clang do
+      build 1200
+      cause "Cannot find terminal libraries - configure failed"
+    end
+  end
+
   def install
-    system "./configure", "--prefix=#{prefix}", "--with-regex=pcre"
+    system "./configure", "--prefix=#{prefix}", "--with-regex=pcre2", "--mandir=#{man}"
     system "make", "install"
   end
 
