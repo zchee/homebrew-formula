604e4281637919a523f14f22f3d04cae71be680b
Koichi Shiraishi <zchee.io@gmail.com>
2020-11-18 12:37:00 +0900

    bats-core: add bats-core formula

---
+++ Formula/bats-core.rb
@@ -0,0 +1,26 @@
+class BatsCore < Formula
+  desc "Bash Automated Testing System"
+  homepage "https://github.com/bats-core/bats-core"
+  head "https://github.com/bats-core/bats-core.git"
+  license "MIT"
+
+  bottle :unneeded
+
+  depends_on "coreutils"
+
+  conflicts_with "bats", because: "both install `bats` executables"
+
+  def install
+    system "./install.sh", prefix
+  end
+
+  test do
+    (testpath/"test.sh").write <<~EOS
+      @test "addition using bc" {
+        result="$(echo 2+2 | bc)"
+        [ "$result" -eq 4 ]
+      }
+    EOS
+    assert_match "addition", shell_output("#{bin}/bats test.sh")
+  end
+end
