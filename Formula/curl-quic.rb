a910ad68bf2213b6b9f277e9c3c8d4617bb5e496
Koichi Shiraishi <zchee.io@gmail.com>
2020-11-24 07:14:22 +0900

    curl-quic: fix install command

--- Formula/curl-quic.rb
+++ Formula/curl-quic.rb
@@ -1,49 +1,55 @@
 class CurlQuic < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server with QUIC"
   homepage "https://curl.haxx.se/"
-  head "https://github.com/curl/curl.git"
+  license "curl"
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "brotli" => :build
-  depends_on "c-ares" => :build
-  depends_on "libidn" => :build
-  depends_on "libmetalink" => :build
-  depends_on "libssh2" => :build
-  depends_on "libtool" => :build
-  depends_on "nghttp2" => :build
-  depends_on "nghttp3" => :build
-  depends_on "ngtcp2" => :build
-  depends_on "openldap" => :build
-  depends_on "openssl-quic" => :build
-  depends_on "pkg-config" => :build
-  depends_on "rtmpdump" => :build
-  depends_on "zstd" => :build
+  bottle :unneeded
+
+  head do
+    url "https://github.com/curl/curl.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+
+    depends_on "brotli" => :build
+    depends_on "c-ares" => :build
+    depends_on "libidn2" => :build
+    depends_on "libmetalink" => :build
+    depends_on "libssh2" => :build
+    depends_on "nghttp2" => :build
+    depends_on "nghttp3" => :build
+    depends_on "ngtcp2" => :build
+    depends_on "openldap" => :build
+    depends_on "openssl-quic" => :build
+    depends_on "pkg-config" => :build
+    depends_on "rtmpdump" => :build
+    depends_on "zstd" => :build
+  end
 
   def install
-    system "autoreconf", "-fi"
-    
-    ENV.append "CFLAGS", "-march=native -Ofast -flto"
-    ENV.append "LDFLAGS", "-march=native -Ofast -flto"
+    system "autoreconf", "-fiv"
+
+    ENV.append "CFLAGS", "-march=native -Ofast -flto=thin"
+    ENV.append "LDFLAGS", "-march=native -Ofast -flto=thin"
     ENV.prepend "CPPFLAGS", "-isystem #{Formula["openssl-quic"].include}"
     ENV.prepend "LDFLAGS", "-L#{Formula["openssl-quic"].lib}"
-
-    ENV["LIBTOOLIZE"] = Formula["libtool"].bin/"glibtool"
     
+    openssl_quic = Formula["openssl-quic"]
     args = %W[
       --disable-debug
       --disable-dependency-tracking
       --disable-silent-rules
       --prefix=#{prefix}
       --enable-ares=#{Formula["c-ares"].opt_prefix}
-      --with-ca-bundle=#{etc}/openssl-quic/cert.pem
-      --with-ca-path=#{etc}/openssl-quic/certs
+      --with-ca-bundle=#{openssl_quic.pkgetc}/cert.pem
+      --with-ca-path=#{openssl_quic.pkgetc}/certs
       --with-gssapi
       --with-libidn2
       --with-libmetalink
       --with-librtmp
       --with-libssh2
-      --with-ssl=#{Formula["openssl-quic"].opt_prefix}
+      --with-ssl=#{openssl_quic.opt_prefix}
       --with-nghttp3=#{Formula["nghttp3"].opt_prefix}
       --with-ngtcp2=#{Formula["ngtcp2"].opt_prefix}
       --without-quiche
@@ -53,7 +59,6 @@ class CurlQuic < Formula
     
     system "./configure", *args
     system "make", "install"
-
     libexec.install "lib/mk-ca-bundle.pl"
   end
 
