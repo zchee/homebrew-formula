60c23c8db51709ba0679d2be836b1dfabf806990
Koichi Shiraishi <zchee.io@gmail.com>
2020-12-17 04:20:59 +0900

    src-cli: add src-cli formula

---
+++ Formula/src-cli.rb
@@ -0,0 +1,28 @@
+class SrcCli < Formula
+  desc "Sourcegraph CLI"
+  homepage "https://sourcegraph.com/"
+  license "Apache-2.0"
+  head "https://github.com/sourcegraph/src-cli.git"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle :unneeded
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = ["-s",
+               "-w",
+               "\"-extldflags=-static\""]
+
+    system "go", "build", "-v", "-o", bin/"src-cli", "-ldflags", ldflags.join(" "), "./cmd/src"
+  end
+
+  test do
+    system "#{bin}/src-cli", "version"
+  end
+end
