4af8246303056ee561b5263061c979044a430415
Koichi Shiraishi <zchee.io@gmail.com>
2020-12-17 01:32:09 +0900

    ngtcp2: use autoconf build instead of cmake

--- Formula/ngtcp2.rb
+++ Formula/ngtcp2.rb
@@ -5,30 +5,25 @@ class Ngtcp2 < Formula
   head do
     url "https://github.com/ngtcp2/ngtcp2.git"
 
-    depends_on "cmake"
+    depends_on "pkg-config"
+    depends_on "autoconf"
+    depends_on "automake"
+    depends_on "libtool"
   end
 
-  depends_on "pkg-config" => :build
+  depends_on "jemalloc"
+  depends_on "libev"
+  depends_on "nghttp3"
   depends_on "openssl-quic"
 
   def install
-    args = std_cmake_args << "-Wno-dev"
-    args << "-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=#{MacOS.version}"
-    args << "-DCMAKE_C_STANDARD=11"
-    args << "-DCMAKE_CXX_STANDARD=17"
-    args << "-DENABLE_GNUTLS:BOOL=OFF"
-    args << "-DENABLE_OPENSSL:BOOL=ON"
-    args << "-DOPENSSL_INCLUDE_DIRS:PATH=#{Formula["openssl-quic"].opt_include}"
-    args << "-DOPENSSL_LIBRARIES:PATH=#{Formula["openssl-quic"].opt_lib}"
-    args << "-DLIBEV_INCLUDE_DIR:PATH="
-    args << "-DLIBEV_LIBRARY:FILEPATH="
-    args << "-DLIBNGHTTP3_INCLUDE_DIR:PATH="
-    args << "-DLIBNGHTTP3_LIBRARY:FILEPATH="
+    ENV.append "CFLAGS", "-march=native -Ofast -flto=thin -std=c11"
+    ENV.append "CXXFLAGS", "-march=native -Ofast -flto=thin -std=c++17 -stdlib=libc++"
+    ENV.append "LDFLAGS", "-march=native -Ofast -flto=thin"
 
-    mkdir "build" do
-      system "cmake", "..", *args
-      system "make"
-      system "make", "install"
-    end
+    system "autoreconf", "-iv"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "check"
+    system "make", "install"
   end
 end
