8d9388efd9892354a9c122caf3381afe371db779
Koichi Shiraishi <zchee.io@gmail.com>
2020-12-17 01:24:03 +0900

    nghttp3: add license and use make check

--- Formula/nghttp3.rb
+++ Formula/nghttp3.rb
@@ -2,6 +2,9 @@ class Nghttp3 < Formula
   desc "HTTP/3 library written in C"
   homepage "https://github.com/ngtcp2/nghttp3"
   head "https://github.com/ngtcp2/nghttp3.git"
+  license "MIT"
+
+  bottle :unneeded
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -10,9 +13,9 @@ class Nghttp3 < Formula
   def install
     ENV.cxx11
 
-    system "autoreconf", "-i"
+    system "autoreconf", "-iv"
     system "./configure", "--prefix=#{prefix}"
-    system "make"
+    system "make", "check"
     system "make", "install"
   end
 end
