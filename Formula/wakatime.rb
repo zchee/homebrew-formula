66b7ed219acb10fa6332413cb5dda805f6f51a73
Koichi Shiraishi <zchee.io@gmail.com>
2020-12-22 23:33:44 +0900

    wakatime: add wakatime formula

---
+++ Formula/wakatime.rb
@@ -0,0 +1,29 @@
+class Wakatime < Formula
+  desc "Command-line interface to the WakaTime api"
+  homepage "https://wakatime.com/"
+  license "BSD-3-Clause"
+  head "https://github.com/wakatime/wakatime.git"
+
+  livecheck do
+    url :stable
+  end
+
+  bottle :unneeded
+
+  depends_on "python@3.9"
+
+  def install
+    xy = Language::Python.major_minor_version "python3"
+    ENV["PYTHONPATH"] = libexec/"lib/python#{xy}/site-packages"
+
+    system "python3", *Language::Python.setup_install_args(libexec)
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
+  end
+
+  test do
+    assert_match "Common interface for the WakaTime api.", shell_output("#{bin}/wakatime --help 2>&1")
+
+    assert_match "error: Missing api key.", shell_output("#{bin}/wakatime --project test 2>&1", 104)
+  end
+end
