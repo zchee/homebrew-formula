9bf63f55fed9fda348869af183371391f513f528
Koichi Shiraishi <zchee.io@gmail.com>
2020-12-30 23:44:10 +0900

    act: add act formula

---
+++ Formula/act.rb
@@ -0,0 +1,29 @@
+class Act < Formula
+  desc "Run your GitHub Actions locally 🚀"
+  homepage "https://github.com/nektos/act"
+  head "https://github.com/nektos/act.git"
+  license "MIT"
+
+  bottle :unneeded
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "build", "VERSION=#{version}"
+    bin.install "dist/local/act"
+  end
+
+  test do
+    system "git", "clone", "https://github.com/stefanzweifel/laravel-github-actions-demo.git"
+    cd "laravel-github-actions-demo" do
+      system "git", "checkout", "v2.0"
+
+      pull_request_jobs = shell_output("#{bin}/act pull_request --list")
+      assert_match "php-cs-fixer", pull_request_jobs
+
+      push_jobs = shell_output("#{bin}/act push --list")
+      assert_match "phpinsights", push_jobs
+      assert_match "phpunit", push_jobs
+    end
+  end
+end
