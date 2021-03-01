8218931cba5227394318f2e2327d1d1cfd8badc9
Koichi Shiraishi <zchee.io@gmail.com>
2021-03-02 04:47:57 +0900

    groff-head: update depends_on and fix build

--- Formula/groff-head.rb
+++ Formula/groff-head.rb
@@ -2,24 +2,27 @@ class GroffHead < Formula
   desc "GNU troff text-formatting system"
   homepage "https://www.gnu.org/software/groff/"
   head "https://git.savannah.gnu.org/git/groff.git"
+  license "GPL-3.0-or-later"
 
-  depends_on "autoconf" if build
-  depends_on "automake" if build
-  depends_on "gawk" if build
-  depends_on "ghostscript" if build
-  depends_on "libtool" if build
-  depends_on "netpbm" if build
-  depends_on "texinfo" if build
+  bottle :unneeded
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "gawk" => :build
+  depends_on "ghostscript" => :build
+  depends_on "netpbm" => :build
+  depends_on "psutils" => :build
+  depends_on "texinfo" => :build
+  depends_on "uchardet" => :build
+
+  uses_from_macos "libiconv"
+  uses_from_macos "perl"
 
   def install
-    ENV.prepend_path "PATH", Formula["autoconf"].bin
-    ENV.prepend_path "PATH", Formula["automake"].bin
-    ENV.prepend_path "PATH", Formula["libtool"].bin
-    ENV.prepend_path "PATH", Formula["texinfo"].bin
-    ENV.prepend_path "PATH", Formula["netpbm"].bin
-
     system "./bootstrap", "--skip-po"
-    system "./configure", "--prefix=#{prefix}", "--without-x", "--with-gs=#{Formula["ghostscript"].bin}/gs", "--with-awk=#{Formula["gawk"].bin}/gawk"
+    system "./configure", "--prefix=#{prefix}", "--without-x", "--with-uchardet", "--with-gs=#{Formula["ghostscript"].bin}/gs", "--with-awk=#{Formula["gawk"].bin}/gawk"
     system "make" # Separate steps required
     system "make", "install"
   end
