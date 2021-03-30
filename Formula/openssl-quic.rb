2088b337b31ca794e998c5b60fcf5e847fff80c6
Koichi Shiraishi <zchee.io@gmail.com>
2021-03-30 10:33:34 +0900

    openssl-quic: update github repository to quictls/openssl

--- Formula/openssl-quic.rb
+++ Formula/openssl-quic.rb
@@ -1,13 +1,36 @@
 class OpensslQuic < Formula
   desc "Cryptography and SSL/TLS Toolkit with QUIC"
-  homepage "https://github.com/tatsuhiro-t/openssl/tree/post-handshake-data-3.0.0"
-  url "https://github.com/tatsuhiro-t/openssl/archive/post-handshake-data-3.0.0.tar.gz"
+  homepage "https://github.com/quictls/openssl"
+  head "https://github.com/quictls/openssl.git", :branch => "openssl-3.0.0-alpha13+quic"
   license "OpenSSL"
-  version "post-handshake-data-3.0.0"
 
   bottle :unneeded
 
   keg_only :shadowed_by_macos, "macOS provides LibreSSL"
+
+  on_linux do
+    resource "cacert" do
+      # homepage "http://curl.haxx.se/docs/caextract.html"
+      url "https://curl.haxx.se/ca/cacert-2020-01-01.pem"
+      mirror "https://gist.githubusercontent.com/dawidd6/16d94180a019f31fd31bc679365387bc/raw/ef02c78b9d6427585d756528964d18a2b9e318f7/cacert-2020-01-01.pem"
+      sha256 "adf770dfd574a0d6026bfaa270cb6879b063957177a991d453ff1d302c02081f"
+    end
+
+    resource "Test::Harness" do
+      url "https://cpan.metacpan.org/authors/id/L/LE/LEONT/Test-Harness-3.42.tar.gz"
+      sha256 "0fd90d4efea82d6e262e6933759e85d27cbcfa4091b14bf4042ae20bab528e53"
+    end
+
+    resource "Test::More" do
+      url "https://cpan.metacpan.org/authors/id/E/EX/EXODIST/Test-Simple-1.302175.tar.gz"
+      sha256 "c8c8f5c51ad6d7a858c3b61b8b658d8e789d3da5d300065df0633875b0075e49"
+    end
+
+    resource "ExtUtils::MakeMaker" do
+      url "https://cpan.metacpan.org/authors/id/B/BI/BINGOS/ExtUtils-MakeMaker-7.48.tar.gz"
+      sha256 "94e64a630fc37e80c0ca02480dccfa5f2f4ca4b0dd4eeecc1d65acd321c68289"
+    end
+  end
 
   # SSLv2 died with 1.1.0, so no-ssl2 no longer required.
   # SSLv3 & zlib are off by default with 1.1.0 but this may not
@@ -17,6 +40,9 @@ class OpensslQuic < Formula
     args = %W[
       --prefix=#{prefix}
       --openssldir=#{openssldir}
+      no-ssl3
+      no-ssl3-method
+      no-zlib
       enable-tls1_3
     ]
     on_linux do
@@ -65,6 +91,7 @@ class OpensslQuic < Formula
     system "perl", "./Configure", *(configure_args + arch_args)
     system "make"
     system "make", "install", "MANDIR=#{man}", "MANSUFFIX=ssl-quic"
+    system "make", "test"
   end
 
   def openssldir
@@ -77,7 +104,10 @@ class OpensslQuic < Formula
   end
 
   def macos_post_install
+    ohai "Regenerating CA certificate bundle from keychain, this may take a while..."
+
     keychains = %w[
+      /Library/Keychains/System.keychain
       /System/Library/Keychains/SystemRootCertificates.keychain
     ]
 
@@ -86,8 +116,9 @@ class OpensslQuic < Formula
       /-----BEGIN CERTIFICATE-----.*?-----END CERTIFICATE-----/m,
     )
 
+    # Check that the certificate has not expired
     valid_certs = certs.select do |cert|
-      IO.popen("#{bin}/openssl x509 -inform pem -checkend 0 -noout >/dev/null", "w") do |openssl_io|
+      IO.popen("#{bin}/openssl x509 -inform pem -checkend 0 -noout &>/dev/null", "w") do |openssl_io|
         openssl_io.write(cert)
         openssl_io.close_write
       end
@@ -95,8 +126,25 @@ class OpensslQuic < Formula
       $CHILD_STATUS.success?
     end
 
+    # Check that the certificate is trusted in keychain
+    trusted_certs = begin
+      tmpfile = Tempfile.new
+
+      valid_certs.select do |cert|
+        tmpfile.rewind
+        tmpfile.write cert
+        tmpfile.truncate cert.size
+        tmpfile.flush
+        IO.popen("/usr/bin/security verify-cert -l -L -R offline -c #{tmpfile.path} &>/dev/null")
+
+        $CHILD_STATUS.success?
+      end
+    ensure
+      tmpfile&.close!
+    end
+
     openssldir.mkpath
-    (openssldir/"cert.pem").atomic_write(valid_certs.join("\n") << "\n")
+    (openssldir/"cert.pem").atomic_write(trusted_certs.join("\n") << "\n")
   end
 
   def linux_post_install
@@ -106,32 +154,6 @@ class OpensslQuic < Formula
     rm_f openssldir/"cert.pem"
     filename = Pathname.new(cacert.url).basename
     openssldir.install cacert.files(filename => "cert.pem")
-  end
-
-  def caveats
-    <<~EOS
-      A CA file has been bootstrapped using certificates from the system
-      keychain. To add additional certificates, place .pem files in
-        #{openssldir}/certs
-
-      and run
-        #{opt_bin}/c_rehash
-    EOS
-  end
-
-  test do
-    # Make sure the necessary .cnf file exists, otherwise OpenSSL gets moody.
-    assert_predicate pkgetc/"openssl.cnf", :exist?,
-            "OpenSSL requires the .cnf file for some functionality"
-
-    # Check OpenSSL itself functions as expected.
-    (testpath/"testfile.txt").write("This is a test file")
-    expected_checksum = "e2d0fe1585a63ec6009c8016ff8dda8b17719a637405a4e23c0ff81339148249"
-    system bin/"openssl", "dgst", "-sha256", "-out", "checksum.txt", "testfile.txt"
-    open("checksum.txt") do |f|
-      checksum = f.read(100).split("=").last.strip
-      assert_equal checksum, expected_checksum
-    end
   end
 
   def caveats
