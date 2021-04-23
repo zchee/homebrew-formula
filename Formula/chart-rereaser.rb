4deba4ba2ae8507c34aa91266c84a60b77e17714
Koichi Shiraishi <zchee.io@gmail.com>
2021-04-23 15:48:45 +0900

    chart-rereaser: add chart-rereaser formula

---
+++ Formula/chart-rereaser.rb
@@ -0,0 +1,23 @@
+class ChartRereaser < Formula
+  desc "Hosting Helm Charts via GitHub Pages and Releases"
+  homepage "https://github.com/helm/chart-releaser/"
+  bottle :unneeded
+  head "https://github.com/helm/chart-releaser.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s
+      -w
+      "-extldflags=-static"
+      -X=github.com/helm/chart-releaser/cr/cmd.GitCommit=#{Utils.git_head}
+      -X=github.com/helm/chart-releaser/cr/cmd.BuildDate=#{Date.today}
+    ].join(" ")
+    system "go", "build", *std_go_args, "-ldflags", ldflags, "-o", bin/"cr", "./cr/main.go"
+  end
+
+  test do
+    system "#{bin}/cr version"
+  end
+end
