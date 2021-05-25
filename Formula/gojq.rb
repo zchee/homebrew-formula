e34a0bf3ab4b4ae578c905b24ffc5eee89c974c4
Koichi Shiraishi <zchee.io@gmail.com>
2021-05-25 09:12:31 +0900

    gojq: add gojq formula

---
+++ Formula/gojq.rb
@@ -0,0 +1,24 @@
+class Gojq < Formula
+  desc "Pure Go implementation of jq"
+  homepage "https://github.com/itchyny/gojq"
+  license "MIT"
+  head "https://github.com/itchyny/gojq.git", :branch => "main"
+
+  bottle :unneeded
+
+  depends_on "go" => :build
+
+  def install
+    revision = Utils.git_short_head
+    ldflags = %W[
+      -s -w
+      -X github.com/itchyny/gojq/cli.revision=#{revision}
+    ]
+    system "go", "build", "-ldflags", ldflags.join(" "), *std_go_args, "./cmd/gojq"
+    zsh_completion.install "_gojq"
+  end
+
+  test do
+    assert_equal "2\n", pipe_output("#{bin}/gojq .bar", '{"foo":1, "bar":2}')
+  end
+end
