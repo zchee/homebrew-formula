aa2189d95be3ea9f97982a7261c320fc8eb62b0a
Koichi Shiraishi <zchee.io@gmail.com>
2021-09-10 04:54:26 +0900

    radare2: add radare2 formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/radare2.rb
@@ -0,0 +1,34 @@
+class Radare2 < Formula
+  desc "Reverse engineering framework"
+  homepage "https://radare.org"
+  license "LGPL-3.0-only"
+  head "https://github.com/radareorg/radare2.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle :unused
+
+  def install
+    ENV.append "CFLAGS", "-march=native -Ofast -flto -arch x86_64"
+    ENV.append "CXXFLAGS", "-march=native -Ofast -flto -arch x86_64"
+
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --with-capstone5
+      --with-openssl
+      --with-rpath
+    ]
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    assert_match "radare2 #{version}", shell_output("#{bin}/r2 -v")
+  end
+end
