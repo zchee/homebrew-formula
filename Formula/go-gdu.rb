7f0615c7a8d38535ac29c591891f12f0e07b4f39
Koichi Shiraishi <zchee.io@gmail.com>
2021-09-30 06:21:48 +0900

    go-gdu: add gdu head formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/go-gdu.rb
@@ -0,0 +1,34 @@
+class GoGdu < Formula
+  desc "Disk usage analyzer with console interface written in Go"
+  homepage "https://github.com/dundee/gdu"
+  head "https://github.com/dundee/gdu.git"
+  license "MIT"
+
+  bottle :unneeded
+
+  depends_on "go" => :build
+
+  def install
+    user = Utils.safe_popen_read("id", "-u", "-n")
+    major = version.major
+
+    ldflags = %W[
+      -s -w
+      -X "github.com/dundee/gdu/v#{major}/build.Version=v#{version}"
+      -X "github.com/dundee/gdu/v#{major}/build.Time=#{time}"
+      -X "github.com/dundee/gdu/v#{major}/build.User=#{user}"
+    ].join(" ")
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/gdu"
+  end
+
+  test do
+    mkdir_p testpath/"test_dir"
+    (testpath/"test_dir"/"file1").write "hello"
+    (testpath/"test_dir"/"file2").write "brew"
+
+    assert_match version.to_s, shell_output("#{bin}/gdu -v")
+    assert_match "colorized", shell_output("#{bin}/gdu --help 2>&1")
+    assert_match "4.0 KiB file1", shell_output("#{bin}/gdu --non-interactive --no-progress #{testpath}/test_dir 2>&1")
+  end
+end
