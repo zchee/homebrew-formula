62753c9b2e0f043e31698e1743689c7982532a54
Koichi Shiraishi <zchee.io@gmail.com>
2021-09-30 06:35:04 +0900

    wakatime-cli: add wakatime/wakatime-cli formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/wakatime-cli.rb
@@ -0,0 +1,38 @@
+class WakatimeCli < Formula
+  desc "Command line interface used by all WakaTime text editor plugins."
+  homepage "https://github.com/wakatime/wakatime-cli"
+  license "BSD-3-Clause"
+  head "https://github.com/wakatime/wakatime-cli.git", branch: "develop"
+
+  bottle :unneeded
+
+  depends_on "go" => :build
+
+  def install
+    os = if OS.mac?
+      "darwin"
+    else
+      "linux"
+    end
+    arch = Hardware::CPU.arm? ? "arm64" : "amd64"
+
+    ldflags = %W[
+      -s -w
+      -X "github.com/wakatime/wakatime-cli/pkg/version.BuildDate=#{time}"
+      -X "github.com/wakatime/wakatime-cli/pkg/version.Commit=#{version}"
+      -X "github.com/wakatime/wakatime-cli/pkg/version.Version=<Homebrew-build>"
+      -X "github.com/wakatime/wakatime-cli/pkg/version.OS=#{os}"
+      -X "github.com/wakatime/wakatime-cli/pkg/version.Arch=#{arch}"
+    ].join(" ")
+
+    ENV["CGO_ENABLED"] = "0"
+    system "go", "build", *std_go_args(ldflags: ldflags)
+    bin.install_symlink "#{bin}/wakatime-cli" => "#{bin}/wakatime"
+  end
+
+  test do
+    assert_match "Command line interface used by all WakaTime text editor plugins.", shell_output("#{bin}/wakatime-cli --help 2>&1")
+
+    assert_match "error: Missing api key.", shell_output("#{bin}/wakatime --project test --today 2>&1", 104)
+  end
+end
