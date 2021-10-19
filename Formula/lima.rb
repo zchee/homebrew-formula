715b3dda240419c9a13319ebe328e9b8151e7d49
Koichi Shiraishi <zchee.io@gmail.com>
2021-10-19 19:11:28 +0900

    lima: add lima head formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/lima.rb
@@ -0,0 +1,28 @@
+class Lima < Formula
+  desc "Linux virtual machines"
+  homepage "https://github.com/lima-vm/lima"
+  head "https://github.com/lima-vm/lima.git", branch: "master"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+  depends_on "qemu"
+
+  def install
+    system "make", "VERSION=#{version}", "clean", "binaries"
+
+    bin.install Dir["_output/bin/*"]
+    share.install Dir["_output/share/*"]
+
+    # Install shell completions
+    output = Utils.safe_popen_read("#{bin}/limactl", "completion", "bash")
+    (bash_completion/"limactl").write output
+    output = Utils.safe_popen_read("#{bin}/limactl", "completion", "zsh")
+    (zsh_completion/"_limactl").write output
+    output = Utils.safe_popen_read("#{bin}/limactl", "completion", "fish")
+    (fish_completion/"limactl.fish").write output
+  end
+
+  test do
+    assert_match "Pruning", shell_output("#{bin}/limactl prune 2>&1")
+  end
+end
