a88ce44b92a69c7abbc4f428b8ca019172edbb3b
Koichi Shiraishi <zchee.io@gmail.com>
2021-10-20 08:03:56 +0900

    youtube-dl-head: rename to youtube-dl-head.rb
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/youtube-dl-head.rb
+++ Formula/youtube-dl-head.rb
@@ -1,9 +1,9 @@
-class YoutubeDl < Formula
+class YoutubeDlHead < Formula
   include Language::Python::Virtualenv
 
   desc "Download YouTube videos from the command-line"
   homepage "https://youtube-dl.org/"
-  head "https://github.com/ytdl-org/youtube-dl.git"
+  head "https://github.com/ytdl-org/youtube-dl.git", branch "master"
   license "Unlicense"
 
   bottle :unneeded
@@ -12,6 +12,9 @@ class YoutubeDl < Formula
 
   def install
     virtualenv_install_with_resources
+    man1.install_symlink libexec/"share/man/man1/youtube-dl.1" => "youtube-dl.1"
+    bash_completion.install libexec/"etc/bash_completion.d/youtube-dl.bash-completion"
+    fish_completion.install libexec/"etc/fish/completions/youtube-dl.fish"
   end
 
   test do
