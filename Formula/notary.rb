0f07ff6f751ab99b32d16dc5ff724c8fd464fa3e
Koichi Shiraishi <zchee.io@gmail.com>
2021-12-17 06:07:00 +0900

    notary: add notary formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/notary.rb
@@ -0,0 +1,12 @@
+class Notary < Formula
+  desc "Trust over arbitrary collections of data"
+  homepage "https://github.com/notaryproject/notary"
+  head "https://github.com/notaryproject/notary.git", branch: "master"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", "#{bin}/notary", "-tags=pkcs11", "./cmd/notary"
+  end
+end
