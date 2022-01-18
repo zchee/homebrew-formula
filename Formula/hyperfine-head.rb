0bad6c80ee9bc88f571d309df361c21743ca60f8
Koichi Shiraishi <zchee.io@gmail.com>
2022-01-18 18:08:13 +0900

    hyperfine-head: add hyperfine-head formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/hyperfine-head.rb
@@ -0,0 +1,22 @@
+class HyperfineHead < Formula
+  desc "Command-line benchmarking tool"
+  homepage "https://github.com/sharkdp/hyperfine"
+  head "https://github.com/sharkdp/hyperfine.git"
+  license any_of: ["Apache-2.0", "MIT"]
+
+  depends_on "rust" => :build
+
+  def install
+    ENV["SHELL_COMPLETIONS_DIR"] = buildpath
+    system "cargo", "install", *std_cargo_args
+    man1.install "doc/hyperfine.1"
+    bash_completion.install "hyperfine.bash"
+    fish_completion.install "hyperfine.fish"
+    zsh_completion.install "_hyperfine"
+  end
+
+  test do
+    output = shell_output("#{bin}/hyperfine 'sleep 0.3'")
+    assert_match "Benchmark 1: sleep", output
+  end
+end
