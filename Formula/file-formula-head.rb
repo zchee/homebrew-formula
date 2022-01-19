514f4bce9f370715a113f7fb0ee98a50962629fd
Koichi Shiraishi <zchee.io@gmail.com>
2022-01-20 02:14:13 +0900

    file-formula-head: update formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/file-formula-head.rb
+++ Formula/file-formula-head.rb
@@ -1,37 +1,41 @@
 # "File" is a reserved class name
 class FileFormulaHead < Formula
   desc "Utility to determine file types"
-  homepage "https://www.darwinsys.com/file/"
-  head "https://github.com/file/file.git"
+  homepage "https://darwinsys.com/file/"
+  # file-formula has a BSD-2-Clause-like license
+  license :cannot_represent
+  head "https://github.com/file/file.git", branch: "master"
+
+  livecheck do
+    url "https://astron.com/pub/file/"
+    regex(/href=.*?file[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
 
   keg_only :provided_by_macos
+
+  depends_on "libmagic"
+  depends_on "bzip2"
+  depends_on "xz"
+  depends_on "zlib"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on "libmagic" => :build
-  depends_on "zlib" => :build
-  depends_on "xz" => :build
 
   def install
     ENV.prepend "LDFLAGS", "-L#{Formula["libmagic"].opt_lib} -lmagic"
 
-    args = %W[
-      --prefix=#{prefix}
-      --disable-dependency-tracking
-      --enable-static
-      --enable-shared
-    ]
+    system "autoreconf", "-fi"
+    inreplace "src/Makefile.in" do |s|
+      s.gsub! "file_DEPENDENCIES = libmagic.la", ""
+      s.gsub! "file_LDADD = libmagic.la", "file_LDADD = $(LDADD)"
+    end
 
-    system "autoreconf", "-ivf"
-
-    inreplace "src/Makefile.in", "file_DEPENDENCIES = libmagic.la", ""
-    inreplace "src/Makefile.in", "file_LDADD = libmagic.la", "file_LDADD = $(LDADD)"
-
-    system "./configure", *args
-
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make", "install-exec"
     system "make", "-C", "doc", "install-man1"
+    rm_r lib
   end
 
   test do
