5c2c908e0e1b53018da5798e00d93a9ff8935233
Koichi Shiraishi <zchee.io@gmail.com>
2022-03-23 17:34:57 +0900

    zsh-head: update zsh-head formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/zsh-head.rb
+++ Formula/zsh-head.rb
@@ -3,8 +3,7 @@ class ZshHead < Formula
   homepage "https://www.zsh.org/"
 
   head do
-    url "https://git.code.sf.net/p/zsh/code.git"
-    mirror "https://github.com/zsh-users/zsh.git"
+    url "https://github.com/zsh-users/zsh.git"
 
     depends_on "autoconf"
     depends_on "gdbm"
@@ -34,6 +33,10 @@ class ZshHead < Formula
     ENV.append "CPPFLAGS", "-D_DARWIN_C_SOURCE -I#{Formula["ncurses-head"].opt_include}/ncursesw"
     ENV.append "LDFLAGS", "-L#{Formula["ncurses-head"].opt_lib} -lncursesw"
 
+    puts "CFLAGS=#{ENV["CFLAGS"]}"
+    puts "LDFLAGS=#{ENV["LDFLAGS"]}"
+    puts "CPPFLAGS=#{ENV["CPPFLAGS"]}"
+
     system "Util/preconfig" if build.head?
 
     system "./configure", "--prefix=#{prefix}",
@@ -47,68 +50,62 @@ class ZshHead < Formula
            "--enable-multibyte",
            "--enable-pcre",
            "--enable-zsh-secure-free",
+           "--enable-stack-allocation",
            "--enable-unicode9",
            "--disable-etcdir",
            "--with-tcsetpgrp",
            "--disable-dynamic"
 
-    inreplace "config.modules", "auto=yes", "auto=no"
     inreplace "config.modules" do |s|
-      s.gsub! "name=zsh/main modfile=Src/zsh.mdd link=static auto=no load=yes", "name=zsh/main modfile=Src/zsh.mdd link=static auto=no load=yes"
-      # s.gsub! "name=zsh/rlimits modfile=Src/Builtins/rlimits.mdd link=static auto=no load=yes"  # enabled by default
-      # s.gsub! "name=zsh/sched modfile=Src/Builtins/sched.mdd link=static auto=no load=yes"  # enabled by default
-      s.gsub! "name=zsh/attr modfile=Src/Modules/attr.mdd link=no auto=no load=no", "name=zsh/attr modfile=Src/Modules/attr.mdd link=static auto=no load=yes"
-      # s.gsub! "name=zsh/cap modfile=Src/Modules/cap.mdd link=no auto=no load=no", "name=zsh/cap modfile=Src/Modules/cap.mdd link=static auto=no load=yes"
-      # s.gsub! "name=zsh/clone modfile=Src/Modules/clone.mdd link=no auto=no load=no"  # disabled by default
-      s.gsub! "name=zsh/curses modfile=Src/Modules/curses.mdd link=no auto=no load=no", "name=zsh/curses modfile=Src/Modules/curses.mdd link=static auto=no load=yes"
-      s.gsub! "name=zsh/datetime modfile=Src/Modules/datetime.mdd link=static auto=no load=no functions=Functions/Calendar/*", "name=zsh/datetime modfile=Src/Modules/datetime.mdd link=static auto=no load=yes functions=Functions/Calendar/*"
-      s.gsub! "name=zsh/db/gdbm modfile=Src/Modules/db_gdbm.mdd link=no auto=no load=no", "name=zsh/db/gdbm modfile=Src/Modules/db_gdbm.mdd link=static auto=no load=yes"
-      # s.gsub! "name=zsh/example modfile=Src/Modules/example.mdd link=no auto=no load=no"  # disabled by default
-      s.gsub! "name=zsh/files modfile=Src/Modules/files.mdd link=no auto=no load=no", "name=zsh/files modfile=Src/Modules/files.mdd link=static auto=no load=yes"
-      s.gsub! "name=zsh/langinfo modfile=Src/Modules/langinfo.mdd link=static auto=no load=no", "name=zsh/langinfo modfile=Src/Modules/langinfo.mdd link=static auto=no load=yes"
-      s.gsub! "name=zsh/mapfile modfile=Src/Modules/mapfile.mdd link=no auto=no load=no", "name=zsh/mapfile modfile=Src/Modules/mapfile.mdd link=static auto=no load=yes"
-      s.gsub! "name=zsh/mathfunc modfile=Src/Modules/mathfunc.mdd link=no auto=no load=no", "name=zsh/mathfunc modfile=Src/Modules/mathfunc.mdd link=static auto=no load=yes"
-      # s.gsub! "name=zsh/nearcolor modfile=Src/Modules/nearcolor.mdd link=no auto=no load=no"  # disabled by default
-      # s.gsub! "name=zsh/newuser modfile=Src/Modules/newuser.mdd link=no auto=no load=no functions=Scripts/newuser Functions/Newuser/*"  # disabled by default
-      s.gsub! "name=zsh/param/private modfile=Src/Modules/param_private.mdd link=no auto=no load=no", "name=zsh/param/private modfile=Src/Modules/param_private.mdd link=static auto=no load=yes"
-      s.gsub! "name=zsh/pcre modfile=Src/Modules/pcre.mdd link=no auto=no load=no", "name=zsh/pcre modfile=Src/Modules/pcre.mdd link=static auto=no load=yes"
-      s.gsub! "name=zsh/regex modfile=Src/Modules/regex.mdd link=no auto=no load=no", "name=zsh/regex modfile=Src/Modules/regex.mdd link=static auto=no load=yes"
-      s.gsub! "name=zsh/net/socket modfile=Src/Modules/socket.mdd link=no auto=no load=no", "name=zsh/net/socket modfile=Src/Modules/socket.mdd link=static auto=no load=yes"
-      s.gsub! "name=zsh/stat modfile=Src/Modules/stat.mdd link=no auto=no load=no", "name=zsh/stat modfile=Src/Modules/stat.mdd link=static auto=no load=yes"
-      s.gsub! "name=zsh/system modfile=Src/Modules/system.mdd link=no auto=no load=no", "name=zsh/system modfile=Src/Modules/system.mdd link=static auto=no load=yes"
-      # s.gsub! "name=zsh/net/tcp modfile=Src/Modules/tcp.mdd link=no auto=no load=no", "name=zsh/net/tcp modfile=Src/Modules/tcp.mdd link=static auto=no load=yes"
-      # s.gsub! "name=zsh/termcap modfile=Src/Modules/termcap.mdd link=static auto=no load=yes"  # enabled by default
-      # s.gsub! "name=zsh/terminfo modfile=Src/Modules/terminfo.mdd link=static auto=no load=yes"  # enabled by default
-      # s.gsub! "name=zsh/zftp modfile=Src/Modules/zftp.mdd link=no auto=no load=no functions=Functions/Zftp/*"  # disabled by default
-      s.gsub! "name=zsh/zprof modfile=Src/Modules/zprof.mdd link=no auto=no load=no", "name=zsh/zprof modfile=Src/Modules/zprof.mdd link=static auto=no load=yes"
-      s.gsub! "name=zsh/zpty modfile=Src/Modules/zpty.mdd link=no auto=no load=no", "name=zsh/zpty modfile=Src/Modules/zpty.mdd link=static auto=no load=yes"
-      s.gsub! "name=zsh/zselect modfile=Src/Modules/zselect.mdd link=no auto=no load=no", "name=zsh/zselect modfile=Src/Modules/zselect.mdd link=static auto=no load=yes"
-      s.gsub! "name=zsh/rlimits modfile=Src/Builtins/rlimits.mdd link=static auto=no load=yes", "name=zsh/rlimits modfile=Src/Builtins/rlimits.mdd link=static auto=no load=yes"
-      s.gsub! "name=zsh/rlimits modfile=Src/Builtins/rlimits.mdd link=static auto=no load=yes", "name=zsh/rlimits modfile=Src/Builtins/rlimits.mdd link=static auto=no load=yes"
-      s.gsub! "name=zsh/rlimits modfile=Src/Builtins/rlimits.mdd link=static auto=no load=yes", "name=zsh/rlimits modfile=Src/Builtins/rlimits.mdd link=static auto=no load=yes"
-      s.gsub! "name=zsh/rlimits modfile=Src/Builtins/rlimits.mdd link=static auto=no load=yes", "name=zsh/rlimits modfile=Src/Builtins/rlimits.mdd link=static auto=no load=yes"
-      s.gsub! "name=zsh/rlimits modfile=Src/Builtins/rlimits.mdd link=static auto=no load=yes", "name=zsh/rlimits modfile=Src/Builtins/rlimits.mdd link=static auto=no load=yes"
-      # s.gsub! "config.modules", "name=zsh/zutil modfile=Src/Modules/zutil.mdd link=static auto=no load=yes"  # enabled by default
-      # s.gsub! "config.modules", "name=zsh/compctl modfile=Src/Zle/compctl.mdd link=static auto=no load=yes"  # enabled by default
-      # s.gsub! "config.modules", "name=zsh/complete modfile=Src/Zle/complete.mdd link=static auto=no load=yes functions=Completion/*comp* Completion/AIX/*/* Completion/BSD/*/* Completion/Base/*/* Completion/Cygwin/*/* Completion/Darwin/*/* Completion/Debian/*/* Completion/Linux/*/* Completion/Mandriva/*/* Completion/Redhat/*/* Completion/Solaris/*/* Completion/openSUSE/*/* Completion/Unix/*/* Completion/X/*/* Completion/Zsh/*/*"  # enable by default
-      # s.gsub! "config.modules", "name=zsh/complist modfile=Src/Zle/complist.mdd link=static auto=no load=yes"  # enabled by default
-      # s.gsub! "config.modules", "name=zsh/computil modfile=Src/Zle/computil.mdd link=static auto=no load=yes"  # enabled by default
-      # s.gsub! "config.modules", "name=zsh/deltochar modfile=Src/Zle/deltochar.mdd link=no auto=no load=no"  # disabled by default
-      # s.gsub! "config.modules", "name=zsh/zle modfile=Src/Zle/zle.mdd link=static auto=no load=yes functions=Functions/Zle/*"  # enabled by default
-      # s.gsub! "config.modules", "name=zsh/zleparameter modfile=Src/Zle/zleparameter.mdd link=static auto=no load=yes"  # enabled by default
+      # s.gsub! "name=zsh/main modfile=Src/zsh.mdd link=static auto=yes load=yes", "name=zsh/main modfile=Src/zsh.mdd link=static auto=no load=yes"
+      # s.gsub! "name=zsh/rlimits modfile=Src/Builtins/rlimits.mdd link=static auto=yes load=yes"  # enabled by default
+      # s.gsub! "name=zsh/sched modfile=Src/Builtins/sched.mdd link=static auto=yes load=yes"  # enabled by default
+      s.gsub! "name=zsh/attr modfile=Src/Modules/attr.mdd link=no auto=yes load=no", "name=zsh/attr modfile=Src/Modules/attr.mdd link=static auto=no load=yes"
+      # s.gsub! "name=zsh/cap modfile=Src/Modules/cap.mdd link=no auto=yes load=no", "name=zsh/cap modfile=Src/Modules/cap.mdd link=static auto=no load=yes"
+      # s.gsub! "name=zsh/clone modfile=Src/Modules/clone.mdd link=no auto=yes load=no"  # disabled by default
+      s.gsub! "name=zsh/curses modfile=Src/Modules/curses.mdd link=no auto=yes load=no", "name=zsh/curses modfile=Src/Modules/curses.mdd link=static auto=no load=yes"
+      s.gsub! "name=zsh/datetime modfile=Src/Modules/datetime.mdd link=static auto=yes load=no functions=Functions/Calendar/*", "name=zsh/datetime modfile=Src/Modules/datetime.mdd link=static auto=no load=yes functions=Functions/Calendar/*"
+      s.gsub! "name=zsh/db/gdbm modfile=Src/Modules/db_gdbm.mdd link=no auto=yes load=no", "name=zsh/db/gdbm modfile=Src/Modules/db_gdbm.mdd link=static auto=no load=yes"
+      # s.gsub! "name=zsh/example modfile=Src/Modules/example.mdd link=no auto=yes load=no"  # disabled by default
+      s.gsub! "name=zsh/files modfile=Src/Modules/files.mdd link=no auto=yes load=no", "name=zsh/files modfile=Src/Modules/files.mdd link=static auto=no load=yes"
+      s.gsub! "name=zsh/langinfo modfile=Src/Modules/langinfo.mdd link=static auto=yes load=no", "name=zsh/langinfo modfile=Src/Modules/langinfo.mdd link=static auto=no load=yes"
+      s.gsub! "name=zsh/mapfile modfile=Src/Modules/mapfile.mdd link=no auto=yes load=no", "name=zsh/mapfile modfile=Src/Modules/mapfile.mdd link=static auto=no load=yes"
+      s.gsub! "name=zsh/mathfunc modfile=Src/Modules/mathfunc.mdd link=no auto=yes load=no", "name=zsh/mathfunc modfile=Src/Modules/mathfunc.mdd link=static auto=no load=yes"
+      # s.gsub! "name=zsh/nearcolor modfile=Src/Modules/nearcolor.mdd link=no auto=yes load=no"  # disabled by default
+      # s.gsub! "name=zsh/newuser modfile=Src/Modules/newuser.mdd link=no auto=yes load=no functions=Scripts/newuser Functions/Newuser/*"  # disabled by default
+      s.gsub! "name=zsh/param/private modfile=Src/Modules/param_private.mdd link=no auto=yes load=no", "name=zsh/param/private modfile=Src/Modules/param_private.mdd link=static auto=no load=yes"
+      s.gsub! "name=zsh/pcre modfile=Src/Modules/pcre.mdd link=no auto=yes load=no", "name=zsh/pcre modfile=Src/Modules/pcre.mdd link=static auto=no load=yes"
+      s.gsub! "name=zsh/regex modfile=Src/Modules/regex.mdd link=no auto=yes load=no", "name=zsh/regex modfile=Src/Modules/regex.mdd link=static auto=no load=yes"
+      s.gsub! "name=zsh/net/socket modfile=Src/Modules/socket.mdd link=no auto=yes load=no", "name=zsh/net/socket modfile=Src/Modules/socket.mdd link=static auto=no load=yes"
+      s.gsub! "name=zsh/stat modfile=Src/Modules/stat.mdd link=no auto=yes load=no", "name=zsh/stat modfile=Src/Modules/stat.mdd link=static auto=no load=yes"
+      s.gsub! "name=zsh/system modfile=Src/Modules/system.mdd link=no auto=yes load=no", "name=zsh/system modfile=Src/Modules/system.mdd link=static auto=no load=yes"
+      # s.gsub! "name=zsh/net/tcp modfile=Src/Modules/tcp.mdd link=no auto=no load=yes", "name=zsh/net/tcp modfile=Src/Modules/tcp.mdd link=static auto=no load=yes"
+      s.gsub! "name=zsh/termcap modfile=Src/Modules/termcap.mdd link=static auto=yes load=yes", "name=zsh/termcap modfile=Src/Modules/termcap.mdd link=no auto=no load=no"
+      # s.gsub! "name=zsh/terminfo modfile=Src/Modules/terminfo.mdd link=static auto=yes load=no"  # disabled by default
+      # s.gsub! "name=zsh/zftp modfile=Src/Modules/zftp.mdd link=no auto=no load=yes functions=Functions/Zftp/*"  # disabled by default
+      s.gsub! "name=zsh/zprof modfile=Src/Modules/zprof.mdd link=no auto=yes load=no", "name=zsh/zprof modfile=Src/Modules/zprof.mdd link=static auto=no load=yes"
+      s.gsub! "name=zsh/zpty modfile=Src/Modules/zpty.mdd link=no auto=yes load=no", "name=zsh/zpty modfile=Src/Modules/zpty.mdd link=static auto=no load=yes"
+      s.gsub! "name=zsh/zselect modfile=Src/Modules/zselect.mdd link=no auto=yes load=no", "name=zsh/zselect modfile=Src/Modules/zselect.mdd link=static auto=no load=yes"
+      # s.gsub! "config.modules", "name=zsh/zutil modfile=Src/Modules/zutil.mdd link=static auto=yes load=yes"  # enabled by default
+      # s.gsub! "config.modules", "name=zsh/compctl modfile=Src/Zle/compctl.mdd link=static auto=yes load=yes"  # enabled by default
+      # s.gsub! "config.modules", "name=zsh/complete modfile=Src/Zle/complete.mdd link=static auto=yes load=yes functions=Completion/*comp* Completion/AIX/*/* Completion/BSD/*/* Completion/Base/*/* Completion/Cygwin/*/* Completion/Darwin/*/* Completion/Debian/*/* Completion/Linux/*/* Completion/Mandriva/*/* Completion/Redhat/*/* Completion/Solaris/*/* Completion/openSUSE/*/* Completion/Unix/*/* Completion/X/*/* Completion/Zsh/*/*"  # enable by default
+      # s.gsub! "config.modules", "name=zsh/complist modfile=Src/Zle/complist.mdd link=static auto=yes load=yes"  # enabled by default
+      # s.gsub! "config.modules", "name=zsh/computil modfile=Src/Zle/computil.mdd link=static auto=yes load=yes"  # enabled by default
+      # s.gsub! "config.modules", "name=zsh/deltochar modfile=Src/Zle/deltochar.mdd link=no auto=yes load=no"  # disabled by default
+      # s.gsub! "config.modules", "name=zsh/zle modfile=Src/Zle/zle.mdd link=static auto=yes load=yes functions=Functions/Zle/*"  # enabled by default
+      # s.gsub! "config.modules", "name=zsh/zleparameter modfile=Src/Zle/zleparameter.mdd link=static auto=yes load=yes"  # enabled by default
     end
 
-    system "echo", "config.modules"
-    system "cat", "config.modules"
-    system "echo", "config.h"
-    system "cat", "config.h"
+    # system "echo", "config.modules"
+    # system "cat", "config.modules"
+    # system "echo", "config.h"
+    # system "cat", "config.h"
 
     # Do not version installation directories.
     inreplace ["Makefile", "Src/Makefile"],
               "$(libdir)/$(tzsh)/$(VERSION)", "$(libdir)"
 
     system "make", "install"
-    system "make", "install.bin", "install.modules", "install.fns"
   end
 
   test do
