b8816e2a653a7dd534395ffaefb779cf934c38f6
Koichi Shiraishi <zchee.io@gmail.com>
2022-06-04 00:11:43 +0900

    sshpass: patched term_child function
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -1,14 +1,13 @@
 class Sshpass < Formula
   desc "Non-interactive ssh password auth"
   homepage "http://sourceforge.net/projects/sshpass"
-  url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.06/sshpass-1.06.tar.gz"
-  version "1.06"
-  sha256 "c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60"
-  head "https://svn.code.sf.net/p/sshpass/code/trunk"
+  head "https://git.code.sf.net/p/sshpass/code-git.git", branch: "main"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+
+  patch :DATA
 
   def install
     args = %W[
@@ -26,3 +25,57 @@ class Sshpass < Formula
     system "sshpass"
   end
 end
+
+__END__
+diff --git a/main.c b/main.c
+index 3f65352..af7b30b 100644
+--- a/main.c
++++ b/main.c
+@@ -234,6 +234,23 @@ static int masterpt;
+ int childpid;
+ int termsig;
+ 
++void term_child(int signum)
++{
++    fflush(stdout);
++    switch(signum) {
++    case SIGINT:
++        reliable_write(masterpt, "\x03", 1);
++        break;
++    case SIGTSTP:
++        reliable_write(masterpt, "\x1a", 1);
++        break;
++    default:
++        if( childpid>0 ) {
++            kill( childpid, signum );
++        }
++    }
++}
++
+ int runprogram( int argc, char *argv[] )
+ {
+     struct winsize ttysize; // The size of our tty
+@@ -580,23 +597,6 @@ void term_handler(int signum)
+     termsig = signum;
+ }
+ 
+-void term_child(int signum);
+-{
+-    fflush(stdout);
+-    switch(signum) {
+-    case SIGINT:
+-        reliable_write(masterpt, "\x03", 1);
+-        break;
+-    case SIGTSTP:
+-        reliable_write(masterpt, "\x1a", 1);
+-        break;
+-    default:
+-        if( childpid>0 ) {
+-            kill( childpid, signum );
+-        }
+-    }
+-}
+-
+ void reliable_write( int fd, const void *data, size_t size )
+ {
+     ssize_t result = write( fd, data, size );
