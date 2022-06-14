93822130862f0c7dae30bc93903f7e0b62f3f1a8
Koichi Shiraishi <zchee.io@gmail.com>
2022-06-14 20:41:50 +0900

    fswatch-head: add fswatch-head formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/fswatch-head.rb
@@ -0,0 +1,29 @@
+class FswatchHead < Formula
+  desc "Monitor a directory for changes and run a shell command"
+  homepage "https://github.com/emcrisostomo/fswatch"
+  head "https://github.com/emcrisostomo/fswatch.git", branch: "master"
+  license all_of: ["GPL-3.0-or-later", "Apache-2.0"]
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "gettext" => :build
+  depends_on "libtool" => :build
+
+  def install
+    ENV.cxx11
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"fswatch", "-h"
+  end
+end
