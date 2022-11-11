b8af786562ccc043e0d5f9b85ce9605f1ce5e5df
Koichi Shiraishi <zchee.io@gmail.com>
2022-11-12 02:04:18 +0900

    openssl-quic: update branch to openssl-3.0.7+quic
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/openssl-quic.rb
+++ Formula/openssl-quic.rb
@@ -1,7 +1,7 @@
 class OpensslQuic < Formula
   desc "Cryptography and SSL/TLS Toolkit with QUIC"
   homepage "https://github.com/quictls/openssl"
-  head "https://github.com/quictls/openssl.git", branch: "openssl-3.0.5+quic"
+  head "https://github.com/quictls/openssl.git", branch: "openssl-3.0.7+quic"
   license "OpenSSL"
 
   keg_only :shadowed_by_macos, "macOS provides LibreSSL"
