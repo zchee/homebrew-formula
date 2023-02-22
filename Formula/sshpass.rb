4203ca62152f684c3bad6766c842b91b698e1bf0
Koichi Shiraishi <zchee.io@gmail.com>
2023-02-23 06:34:28 +0900

    sshpass: remove patch DATA
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -7,8 +7,6 @@ class Sshpass < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
 
-  patch :DATA
-
   def install
     args = %W[
       --disable-dependency-tracking
@@ -25,57 +23,3 @@ class Sshpass < Formula
     system "sshpass"
   end
 end
-
-__END__
-diff --git a/main.c b/main.c
-index 3f65352..af7b30b 100644
---- a/main.c
-+++ b/main.c
-@@ -234,6 +234,23 @@ static int masterpt;
- int childpid;
- int termsig;
- 
-+void term_child(int signum)
-+{
-+    fflush(stdout);
-+    switch(signum) {
-+    case SIGINT:
-+        reliable_write(masterpt, "\x03", 1);
-+        break;
-+    case SIGTSTP:
-+        reliable_write(masterpt, "\x1a", 1);
-+        break;
-+    default:
-+        if( childpid>0 ) {
-+            kill( childpid, signum );
-+        }
-+    }
-+}
-+
- int runprogram( int argc, char *argv[] )
- {
-     struct winsize ttysize; // The size of our tty
-@@ -580,23 +597,6 @@ void term_handler(int signum)
-     termsig = signum;
- }
- 
--void term_child(int signum);
--{
--    fflush(stdout);
--    switch(signum) {
--    case SIGINT:
--        reliable_write(masterpt, "\x03", 1);
--        break;
--    case SIGTSTP:
--        reliable_write(masterpt, "\x1a", 1);
--        break;
--    default:
--        if( childpid>0 ) {
--            kill( childpid, signum );
--        }
--    }
--}
--
- void reliable_write( int fd, const void *data, size_t size )
- {
-     ssize_t result = write( fd, data, size );
