7b03ee9c9bf4bdbbb3b33db0c52ef9b8c66ebfd3
Koichi Shiraishi <zchee.io@gmail.com>
2023-03-19 08:03:03 +0900

    less-head: add less-head formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/less-head.rb
@@ -0,0 +1,30 @@
+class LessHead < Formula
+  desc "Pager program similar to more"
+  homepage "https://www.greenwoodsoftware.com/less/index.html"
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    url :homepage
+    regex(/less[._-]v?(\d+(?:\.\d+)*).+?released.+?general use/i)
+  end
+
+  head do
+    url "https://github.com/gwsw/less.git", branch: "master"
+    depends_on "autoconf" => :build
+    depends_on "groff-head" => :build
+    uses_from_macos "perl" => :build
+  end
+
+  depends_on "ncurses"
+  depends_on "pcre2"
+
+  def install
+    system "make", "-f", "Makefile.aut", "distfiles" if build.head?
+    system "./configure", "--prefix=#{prefix}", "--with-regex=pcre2"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/lesskey", "-V"
+  end
+end
