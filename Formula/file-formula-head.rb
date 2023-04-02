be60a87bdb5f2a8303976c5cbcd891139b35b2de
Koichi Shiraishi <zchee.io@gmail.com>
2023-04-03 01:41:47 +0900

    file-formula-head: support zstd and lzip
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/file-formula-head.rb
+++ Formula/file-formula-head.rb
@@ -4,7 +4,13 @@ class FileFormulaHead < Formula
   homepage "https://darwinsys.com/file/"
   # file-formula has a BSD-2-Clause-like license
   license :cannot_represent
-  head "https://github.com/file/file.git", branch: "master"
+  head do
+    url "https://github.com/file/file.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
 
   livecheck do
     url "https://astron.com/pub/file/"
@@ -14,25 +20,25 @@ class FileFormulaHead < Formula
   keg_only :provided_by_macos
 
   depends_on "libmagic"
+  depends_on "zlib"
   depends_on "bzip2"
   depends_on "xz"
-  depends_on "zlib"
-
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
+  depends_on "zstd"
+  depends_on "lzip"
 
   def install
     ENV.prepend "LDFLAGS", "-L#{Formula["libmagic"].opt_lib} -lmagic"
+    ENV.prepend "LDFLAGS", "-L#{Formula["lzip"].opt_lib} -llzip"
 
-    system "autoreconf", "-fi"
+    system "autoreconf", "-fiv" if head?
     inreplace "src/Makefile.in" do |s|
       s.gsub! "file_DEPENDENCIES = libmagic.la", ""
-      s.gsub! "file_LDADD = libmagic.la", "file_LDADD = $(LDADD)"
+      s.gsub! "file_LDADD = libmagic.la -lm", "file_LDADD = $(LDADD) -lm"
     end
 
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
+                          "--enable-lzlib"
     system "make", "install-exec"
     system "make", "-C", "doc", "install-man1"
     rm_r lib
