8a366716a5f2fa3652ceb7d28b1ca54330335631
Koichi Shiraishi <zchee.io@gmail.com>
2023-05-07 13:03:22 +0900

    hyperfine-head: use std env rustup for nightly build
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/hyperfine-head.rb
+++ Formula/hyperfine-head.rb
@@ -3,12 +3,24 @@ class HyperfineHead < Formula
   homepage "https://github.com/sharkdp/hyperfine"
   head "https://github.com/sharkdp/hyperfine.git", branch: "master"
   license any_of: ["Apache-2.0", "MIT"]
+  license any_of: ["Apache-2.0", "MIT"]
+  head "https://github.com/sharkdp/hyperfine.git", branch: "master"
 
-  depends_on "rust" => :build
+  env :std
 
   def install
+    # setup nightly cargo with rustup
+    ENV.append_path "PATH", "/usr/local/rust/rustup/bin"
+    ENV["RUSTUP_HOME"] = "/usr/local/rust/rustup"
+    ENV["RUSTFLAGS"] = "-C target-cpu=native -C target-cpu=x86-64-v4 -C target-feature=+aes,+avx,+avx2,+avx512f,+avx512dq,+avx512cd,+avx512bw,+avx512vl"
+
+    # avoid invalid data in index - calculated checksum does not match expected
+    system "git", "config", "--local", "index.skipHash", "false"
+
     ENV["SHELL_COMPLETIONS_DIR"] = buildpath
-    system "cargo", "install", *std_cargo_args
+
+    system "rustup", "run", "nightly", "cargo", "install", "--all-features", "--root", prefix, "--path", "."
+
     man1.install "doc/hyperfine.1"
     bash_completion.install "hyperfine.bash"
     fish_completion.install "hyperfine.fish"
