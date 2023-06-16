6c9634412ce2277faf4e1c82188c2a3275ede8cf
Koichi Shiraishi <zchee.io@gmail.com>
2023-06-16 16:27:14 +0900

    bear-head: add bear-head formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/bear-head.rb
@@ -0,0 +1,54 @@
+class BearHead < Formula
+  desc "Generate compilation database for clang tooling"
+  homepage "https://github.com/rizsotto/Bear"
+  license "GPL-3.0-or-later"
+  head "https://github.com/rizsotto/Bear.git", branch: "master"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "fmt"
+  depends_on "grpc"
+  depends_on "nlohmann-json"
+  depends_on "spdlog"
+
+  uses_from_macos "llvm" => :test
+
+  on_macos do
+    depends_on "llvm" if DevelopmentTools.clang_build_version <= 1100
+  end
+
+  fails_with gcc: "5" # needs C++17
+
+  fails_with :clang do
+    build 1100
+    cause <<-EOS
+      Undefined symbols for architecture x86_64:
+        "std::__1::__fs::filesystem::__current_path(std::__1::error_code*)"
+    EOS
+  end
+
+  def install
+    ENV.llvm_clang if OS.mac? && (DevelopmentTools.clang_build_version <= 1100)
+
+    args = %w[
+      -DENABLE_UNIT_TESTS=OFF
+      -DENABLE_FUNC_TESTS=OFF
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      int main() {
+        printf("hello, world!\\n");
+        return 0;
+      }
+    EOS
+    system bin/"bear", "--", "clang", "test.c"
+    assert_predicate testpath/"compile_commands.json", :exist?
+  end
+end
