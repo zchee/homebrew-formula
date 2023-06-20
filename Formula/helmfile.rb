a7742fd32ce6515c40d51d480b4a85b6e69afff7
Koichi Shiraishi <zchee.io@gmail.com>
2023-06-21 07:59:34 +0900

    helmfile: add helmfile formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/helmfile.rb
@@ -0,0 +1,22 @@
+class Helmfile < Formula
+  desc "Declaratively deploy your Kubernetes manifests, Kustomize configs, and Charts as Helm releases."
+  homepage "https://github.com/helmfile/helmfile"
+  license "MIT"
+  head "https://github.com/helmfile/helmfile.git", :branch => "main"
+
+  depends_on "go" => :build
+
+  def install
+    revision = Utils.git_short_head
+    ldflags = %W[
+      -s -w
+      -X go.szostok.io/version.version=#{version}
+      -X go.szostok.io/version.commit=#{revision}
+      -X go.szostok.io/version.dirtyBuild=false
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags)
+
+    # Install shell completions
+    generate_completions_from_executable(bin/"helmfile", "completion", base_name: "helmfile")
+  end
+end
