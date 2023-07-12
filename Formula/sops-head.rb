37cffd1b044938e2539c4c92172a9d34703a529f
Koichi Shiraishi <zchee.io@gmail.com>
2023-07-12 09:29:06 +0900

    sops-head: fix repository url and branch name
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/sops-head.rb
+++ Formula/sops-head.rb
@@ -1,28 +1,14 @@
 class SopsHead < Formula
   desc "Editor of encrypted files"
-  homepage "https://github.com/mozilla/sops"
+  homepage "https://github.com/getsops/sops"
   license "MPL-2.0"
-  head "https://github.com/mozilla/sops.git", branch: "develop"
+  head "https://github.com/getsops/sops.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
-    ldflags = %W[
-      -s -w
-      -linkmode=external
-      -buildmode=pie
-      -buildid=
-      "-extldflags=-static-pie -all_load -dead_strip -Wl,-no_deduplicate"
-    ].join(" ")
-
-    tags = %W[
-      osusergo
-      netgo
-      static
-    ].join(",")
-
     ENV["CGO_ENABLED"] = "0"
-    system "go", "build", *std_go_args(ldflags: ldflags), "-tags=#{tags}", "-o", bin/"sops", "go.mozilla.org/sops/v3/cmd/sops"
+    system "go", "build", *std_go_args, "-o", bin/"sops", "./cmd/sops"
     pkgshare.install "example.yaml"
   end
 
