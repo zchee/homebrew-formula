42841cafb9d8381dfc0a8c95bfd80d4d5d95fb69
Koichi Shiraishi <zchee.io@gmail.com>
2023-08-04 05:53:04 +0900

    zsh-head: change inreplace config.modules file to parse all lines
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/zsh-head.rb
+++ Formula/zsh-head.rb
@@ -61,24 +61,50 @@ class ZshHead < Formula
            "--with-tcsetpgrp",
            "--disable-dynamic"
 
+    inreplace "config.modules", \
+      "name=zsh/main modfile=Src/zsh.mdd link=static auto=yes load=yes functions=Functions/Chpwd/* Functions/Exceptions/* Functions/Math/* Functions/Misc/* Functions/MIME/* Functions/Prompts/* Functions/VCS_Info/* Functions/VCS_Info/Backends/*", \
+      "name=zsh/main modfile=Src/zsh.mdd link=static auto=no load=yes functions=Functions/Chpwd/* Functions/Exceptions/* Functions/Math/* Functions/Misc/* Functions/MIME/* Functions/Prompts/* Functions/VCS_Info/* Functions/VCS_Info/Backends/*"
+    inreplace "config.modules", "name=zsh/rlimits modfile=Src/Builtins/rlimits.mdd link=static auto=yes load=yes", "name=zsh/rlimits modfile=Src/Builtins/rlimits.mdd link=static auto=no load=yes"
+    inreplace "config.modules", "name=zsh/sched modfile=Src/Builtins/sched.mdd link=static auto=yes load=yes", "name=zsh/sched modfile=Src/Builtins/sched.mdd link=static auto=no load=yes"
     inreplace "config.modules", "name=zsh/attr modfile=Src/Modules/attr.mdd link=no auto=yes load=no", "name=zsh/attr modfile=Src/Modules/attr.mdd link=static auto=no load=yes"
+    inreplace "config.modules", "name=zsh/cap modfile=Src/Modules/cap.mdd link=no auto=yes load=no", "name=zsh/cap modfile=Src/Modules/cap.mdd link=static auto=no load=yes"
+    inreplace "config.modules", "name=zsh/clone modfile=Src/Modules/clone.mdd link=no auto=yes load=no", "name=zsh/clone modfile=Src/Modules/clone.mdd link=static auto=no load=yes"
     inreplace "config.modules", "name=zsh/curses modfile=Src/Modules/curses.mdd link=no auto=yes load=no", "name=zsh/curses modfile=Src/Modules/curses.mdd link=static auto=no load=yes"
     inreplace "config.modules", "name=zsh/datetime modfile=Src/Modules/datetime.mdd link=static auto=yes load=no functions=Functions/Calendar/*", "name=zsh/datetime modfile=Src/Modules/datetime.mdd link=static auto=no load=yes functions=Functions/Calendar/*"
     inreplace "config.modules", "name=zsh/db/gdbm modfile=Src/Modules/db_gdbm.mdd link=no auto=yes load=no", "name=zsh/db/gdbm modfile=Src/Modules/db_gdbm.mdd link=static auto=no load=yes"
+    inreplace "config.modules", "name=zsh/example modfile=Src/Modules/example.mdd link=no auto=yes load=no", "name=zsh/example modfile=Src/Modules/example.mdd link=no auto=no load=no"
     inreplace "config.modules", "name=zsh/files modfile=Src/Modules/files.mdd link=no auto=yes load=no", "name=zsh/files modfile=Src/Modules/files.mdd link=static auto=no load=yes"
-    inreplace "config.modules", "name=zsh/langinfo modfile=Src/Modules/langinfo.mdd link=static auto=yes load=no", "name=zsh/langinfo modfile=Src/Modules/langinfo.mdd link=static auto=no load=yes"
+    inreplace "config.modules", "name=zsh/ksh93 modfile=Src/Modules/ksh93.mdd link=static auto=yes load=yes", "name=zsh/ksh93 modfile=Src/Modules/ksh93.mdd link=static auto=no load=yes"
+    inreplace "config.modules", "name=zsh/langinfo modfile=Src/Modules/langinfo.mdd link=static auto=yes load=no", "name=zsh/langinfo modfile=Src/Modules/langinfo.mdd link=static auto=no load=no"
     inreplace "config.modules", "name=zsh/mapfile modfile=Src/Modules/mapfile.mdd link=no auto=yes load=no", "name=zsh/mapfile modfile=Src/Modules/mapfile.mdd link=static auto=no load=yes"
     inreplace "config.modules", "name=zsh/mathfunc modfile=Src/Modules/mathfunc.mdd link=no auto=yes load=no", "name=zsh/mathfunc modfile=Src/Modules/mathfunc.mdd link=static auto=no load=yes"
+    inreplace "config.modules", "name=zsh/nearcolor modfile=Src/Modules/nearcolor.mdd link=no auto=yes load=no", "name=zsh/nearcolor modfile=Src/Modules/nearcolor.mdd link=no auto=no load=no"
+    inreplace "config.modules", "name=zsh/newuser modfile=Src/Modules/newuser.mdd link=no auto=yes load=no functions=Scripts/newuser Functions/Newuser/*", "name=zsh/newuser modfile=Src/Modules/newuser.mdd link=no auto=no load=no functions=Scripts/newuser Functions/Newuser/*"
     inreplace "config.modules", "name=zsh/param/private modfile=Src/Modules/param_private.mdd link=no auto=yes load=no", "name=zsh/param/private modfile=Src/Modules/param_private.mdd link=static auto=no load=yes"
+    inreplace "config.modules", "name=zsh/parameter modfile=Src/Modules/parameter.mdd link=static auto=yes load=yes", "name=zsh/parameter modfile=Src/Modules/parameter.mdd link=static auto=no load=yes"
     inreplace "config.modules", "name=zsh/pcre modfile=Src/Modules/pcre.mdd link=no auto=yes load=no", "name=zsh/pcre modfile=Src/Modules/pcre.mdd link=static auto=no load=yes"
     inreplace "config.modules", "name=zsh/regex modfile=Src/Modules/regex.mdd link=no auto=yes load=no", "name=zsh/regex modfile=Src/Modules/regex.mdd link=static auto=no load=yes"
     inreplace "config.modules", "name=zsh/net/socket modfile=Src/Modules/socket.mdd link=no auto=yes load=no", "name=zsh/net/socket modfile=Src/Modules/socket.mdd link=static auto=no load=yes"
     inreplace "config.modules", "name=zsh/stat modfile=Src/Modules/stat.mdd link=no auto=yes load=no", "name=zsh/stat modfile=Src/Modules/stat.mdd link=static auto=no load=yes"
     inreplace "config.modules", "name=zsh/system modfile=Src/Modules/system.mdd link=no auto=yes load=no", "name=zsh/system modfile=Src/Modules/system.mdd link=static auto=no load=yes"
-    inreplace "config.modules", "name=zsh/termcap modfile=Src/Modules/termcap.mdd link=static auto=yes load=yes", "name=zsh/termcap modfile=Src/Modules/termcap.mdd link=no auto=no load=no"
-    inreplace "config.modules", "name=zsh/zprof modfile=Src/Modules/zprof.mdd link=no auto=yes load=no", "name=zsh/zprof modfile=Src/Modules/zprof.mdd link=static auto=no load=yes"
+    inreplace "config.modules", "name=zsh/net/tcp modfile=Src/Modules/tcp.mdd link=no auto=yes load=no functions=Functions/TCP/*", "name=zsh/net/tcp modfile=Src/Modules/tcp.mdd link=static auto=no load=yes functions=Functions/TCP/*"
+    inreplace "config.modules", "name=zsh/termcap modfile=Src/Modules/termcap.mdd link=static auto=yes load=yes", "name=zsh/termcap modfile=Src/Modules/termcap.mdd link=static auto=no load=yes"
+    inreplace "config.modules", "name=zsh/terminfo modfile=Src/Modules/terminfo.mdd link=static auto=yes load=yes", "name=zsh/terminfo modfile=Src/Modules/terminfo.mdd link=static auto=no load=yes"
+    inreplace "config.modules", "name=zsh/watch modfile=Src/Modules/watch.mdd link=no auto=yes load=no", "name=zsh/watch modfile=Src/Modules/watch.mdd link=no auto=no load=no"
+    inreplace "config.modules", "name=zsh/zftp modfile=Src/Modules/zftp.mdd link=no auto=yes load=no functions=Functions/Zftp/*", "name=zsh/zftp modfile=Src/Modules/zftp.mdd link=no auto=no load=no functions=Functions/Zftp/*"
+    inreplace "config.modules", "name=zsh/zprof modfile=Src/Modules/zprof.mdd link=no auto=yes load=no", "name=zsh/zprof modfile=Src/Modules/zprof.mdd link=static auto=no load=no"
     inreplace "config.modules", "name=zsh/zpty modfile=Src/Modules/zpty.mdd link=no auto=yes load=no", "name=zsh/zpty modfile=Src/Modules/zpty.mdd link=static auto=no load=yes"
     inreplace "config.modules", "name=zsh/zselect modfile=Src/Modules/zselect.mdd link=no auto=yes load=no", "name=zsh/zselect modfile=Src/Modules/zselect.mdd link=static auto=no load=yes"
+    inreplace "config.modules", "name=zsh/zutil modfile=Src/Modules/zutil.mdd link=static auto=yes load=yes", "name=zsh/zutil modfile=Src/Modules/zutil.mdd link=static auto=no load=yes"
+    inreplace "config.modules", "name=zsh/compctl modfile=Src/Zle/compctl.mdd link=static auto=yes load=yes", "name=zsh/compctl modfile=Src/Zle/compctl.mdd link=static auto=no load=yes"
+    inreplace "config.modules", \
+      "name=zsh/complete modfile=Src/Zle/complete.mdd link=static auto=yes load=yes functions=Completion/*comp* Completion/AIX/*/* Completion/BSD/*/* Completion/Base/*/* Completion/Cygwin/*/* Completion/Darwin/*/* Completion/Debian/*/* Completion/Linux/*/* Completion/Mandriva/*/* Completion/Redhat/*/* Completion/Solaris/*/* Completion/openSUSE/*/* Completion/Unix/*/* Completion/X/*/* Completion/Zsh/*/*", \
+      "name=zsh/complete modfile=Src/Zle/complete.mdd link=static auto=no load=yes functions=Completion/*comp* Completion/AIX/*/* Completion/BSD/*/* Completion/Base/*/* Completion/Cygwin/*/* Completion/Darwin/*/* Completion/Debian/*/* Completion/Linux/*/* Completion/Mandriva/*/* Completion/Redhat/*/* Completion/Solaris/*/* Completion/openSUSE/*/* Completion/Unix/*/* Completion/X/*/* Completion/Zsh/*/*"
+    inreplace "config.modules", "name=zsh/complist modfile=Src/Zle/complist.mdd link=static auto=yes load=yes", "name=zsh/complist modfile=Src/Zle/complist.mdd link=static auto=no load=yes"
+    inreplace "config.modules", "name=zsh/computil modfile=Src/Zle/computil.mdd link=static auto=yes load=yes", "name=zsh/computil modfile=Src/Zle/computil.mdd link=static auto=no load=yes"
+    inreplace "config.modules", "name=zsh/deltochar modfile=Src/Zle/deltochar.mdd link=no auto=yes load=no", "name=zsh/deltochar modfile=Src/Zle/deltochar.mdd link=static auto=no load=yes"
+    inreplace "config.modules", "name=zsh/zle modfile=Src/Zle/zle.mdd link=static auto=yes load=yes functions=Functions/Zle/*", "name=zsh/zle modfile=Src/Zle/zle.mdd link=static auto=no load=yes functions=Functions/Zle/*"
+    inreplace "config.modules", "name=zsh/zleparameter modfile=Src/Zle/zleparameter.mdd link=static auto=yes load=yes", "name=zsh/zleparameter modfile=Src/Zle/zleparameter.mdd link=static auto=no load=yes"
 
     # Do not version installation directories.
     inreplace ["Makefile", "Src/Makefile"],
