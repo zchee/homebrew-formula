3f5961bafd334f99583e26ec910336b70654bc5b
Koichi Shiraishi <zchee.io@gmail.com>
2023-09-20 18:28:33 +0900

    fq: add fq formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/fq.rb
@@ -0,0 +1,13 @@
+class Fq < Formula
+  desc "jq for binary formats"
+  homepage "https://github.com/wader/fq"
+  license "MIT"
+  head "https://github.com/wader/fq.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "fq"
+    bin.install "fq"
+  end
+end
