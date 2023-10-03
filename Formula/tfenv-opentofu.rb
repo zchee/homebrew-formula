f893e89ce85db1d22c8cc21aec2530abb2893e2e
Koichi Shiraishi <zchee.io@gmail.com>
2023-10-04 03:29:21 +0900

    tfenv-opentofu: add tfenv-opentofu formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/tfenv-opentofu.rb
@@ -0,0 +1,31 @@
+class TfenvOpentofu < Formula
+  desc "Terraform version manager inspired by rbenv"
+  homepage "https://github.com/opentofu/tfenv"
+  license "MIT"
+  head "https://github.com/opentofu/tfenv.git", branch: "add-opentofu-support"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  uses_from_macos "unzip"
+
+  on_macos do
+    depends_on "grep"
+  end
+
+  conflicts_with "terraform", because: "tfenv symlinks terraform binaries"
+  conflicts_with "tfenv", because: "OpenTofu fork"
+
+  def install
+    prefix.install %w[bin lib libexec share]
+  end
+
+  test do
+    assert_match "0.10.0", shell_output("#{bin}/tfenv list-remote")
+    with_env(TFENV_TERRAFORM_VERSION: "0.10.0", TF_AUTO_INSTALL: "false") do
+      assert_equal "0.10.0", shell_output("#{bin}/tfenv version-name").strip
+    end
+  end
+end
