766003459c11c4670aee7069893e27c24c2c40a9
Koichi Shiraishi <zchee.io@gmail.com>
2023-10-21 04:29:10 +0900

    argocd-head: add argocd-head formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/argocd-head.rb
@@ -0,0 +1,35 @@
+class ArgocdHead < Formula
+  desc "GitOps Continuous Delivery for Kubernetes"
+  homepage "https://argoproj.github.io/cd"
+  head "https://github.com/argoproj/argo-cd.git" , branch: "master"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+  depends_on "node" => :build
+  depends_on "yarn" => :build
+
+  def install
+    system "make", "dep-ui-local"
+    with_env(
+      NODE_ENV:        "production",
+      NODE_ONLINE_ENV: "online",
+    ) do
+      system "yarn", "--cwd", "ui", "build"
+    end
+    system "make", "cli-local"
+    bin.install "dist/argocd"
+
+    generate_completions_from_executable(bin/"argocd", "completion", shells: [:bash, :zsh])
+  end
+
+  test do
+    assert_match "argocd controls a Argo CD server",
+      shell_output("#{bin}/argocd --help")
+
+    # Providing argocd with an empty config file returns the contexts table header
+    touch testpath/"argocd-config"
+    (testpath/"argocd-config").chmod 0600
+    assert_match "CURRENT  NAME  SERVER\n",
+      shell_output("#{bin}/argocd context --config ./argocd-config")
+  end
+end
