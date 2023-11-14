0fb6c8f421ac3ac0e40bdc6e3ba774132ef4d2a8
Koichi Shiraishi <zchee.io@gmail.com>
2023-11-15 05:23:54 +0900

    helmfile-head: fix ldflags
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/helmfile-head.rb
+++ Formula/helmfile-head.rb
@@ -1,5 +1,5 @@
 class HelmfileHead < Formula
-  desc "Declaratively deploy your Kubernetes manifests, Kustomize configs, and Charts as Helm releases."
+  desc "Deploy Kubernetes Helm Charts"
   homepage "https://github.com/helmfile/helmfile"
   license "MIT"
   head "https://github.com/helmfile/helmfile.git", :branch => "main"
@@ -10,13 +10,14 @@ class HelmfileHead < Formula
     revision = Utils.git_short_head
     ldflags = %W[
       -s -w
-      -X go.szostok.io/version.version=#{version}
+      -X go.szostok.io/version.version=v#{version}
+      -X go.szostok.io/version.buildDate=#{time.iso8601}
       -X go.szostok.io/version.commit=#{revision}
+      -X go.szostok.io/version.commitDate=#{time.iso8601}
       -X go.szostok.io/version.dirtyBuild=false
     ]
     system "go", "build", *std_go_args(ldflags: ldflags), "-o=#{bin}/helmfile"
 
-    # Install shell completions
     generate_completions_from_executable(bin/"helmfile", "completion", base_name: "helmfile")
   end
 end
