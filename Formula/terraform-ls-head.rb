12e69f81d5645c3efb87c8d0b89dcc731f012a8d
Koichi Shiraishi <zchee.io@gmail.com>
2023-11-21 08:34:20 +0900

    terraform-ls-head: add terraform-ls-head formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/terraform-ls-head.rb
@@ -0,0 +1,44 @@
+class TerraformLsHead < Formula
+  desc "Terraform Language Server"
+  homepage "https://github.com/hashicorp/terraform-ls"
+  license "MPL-2.0"
+  head "https://github.com/hashicorp/terraform-ls.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "go" => :build
+
+  def install
+    revision = Utils.git_short_head
+    ldflags = %W[
+      -s -w
+      -X main.rawVersion=#{revision}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags.join(" ")), "-o=#{bin}/terraform-ls"
+  end
+
+  test do
+    port = free_port
+
+    pid = fork do
+      exec "#{bin}/terraform-ls serve -port #{port} /dev/null"
+    end
+    sleep 2
+
+    begin
+      tcp_socket = TCPSocket.new("localhost", port)
+      tcp_socket.puts <<~EOF
+        Content-Length: 59
+
+        {"jsonrpc":"2.0","method":"initialize","params":{},"id":1}
+      EOF
+      assert_match "Content-Type", tcp_socket.gets("\n")
+    ensure
+      Process.kill("SIGINT", pid)
+      Process.wait(pid)
+    end
+  end
+end
