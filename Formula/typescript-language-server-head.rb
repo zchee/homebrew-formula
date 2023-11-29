189211418a1f20833f6fbfe758687e84c283fcc7
Koichi Shiraishi <zchee.io@gmail.com>
2023-11-29 13:43:42 +0900

    typescript-language-server-head: add typescript-language-server-head
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/typescript-language-server-head.rb
@@ -0,0 +1,45 @@
+require "language/node"
+
+class TypescriptLanguageServerHead < Formula
+  desc "Language Server Protocol implementation for TypeScript wrapping tsserver"
+  homepage "https://github.com/typescript-language-server/typescript-language-server"
+  license all_of: ["MIT", "Apache-2.0"]
+  head "https://github.com/typescript-language-server/typescript-language-server.git", branch: "master"
+
+  depends_on "corepack"
+  depends_on "node"
+  depends_on "typescript"
+
+  def install
+    system "#{Formula["corepack"].bin}/yarn"
+    system "#{Formula["corepack"].bin}/yarn", "run", "compile"
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+
+    node_modules = libexec/"lib/node_modules"
+    typescript = Formula["typescript"].opt_libexec/"lib/node_modules/typescript"
+    ln_sf typescript.relative_path_from(node_modules), node_modules
+
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    require "open3"
+
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+
+    Open3.popen3("#{bin}/typescript-language-server", "--stdio") do |stdin, stdout|
+      stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
+      assert_match(/^Content-Length: \d+/i, stdout.readline)
+    end
+  end
+end
