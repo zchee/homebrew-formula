1e148a401caed142a453b7cbebe88812733e8b82
Koichi Shiraishi <zchee.io@gmail.com>
2024-05-08 22:28:13 +0900

    htop-head: add htop-head formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/htop-head.rb
@@ -0,0 +1,41 @@
+class HtopHead < Formula
+  desc "Improved top (interactive process viewer)"
+  homepage "https://htop.dev/"
+  license "GPL-2.0-or-later"
+  head "https://github.com/htop-dev/htop.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "ncurses" # enables mouse scroll
+
+  on_linux do
+    depends_on "lm-sensors"
+  end
+
+  def install
+    system "./autogen.sh"
+    args = ["--prefix=#{prefix}", "--enable-unicode", "--enable-hwloc", "--enable-year2038"]
+    args << "--enable-sensors" if OS.linux?
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      htop requires root privileges to correctly display all running processes,
+      so you will need to run `sudo htop`.
+      You should be certain that you trust any software you grant root privileges.
+    EOS
+  end
+
+  test do
+    pipe_output("#{bin}/htop", "q", 0)
+  end
+end
