2faf365c22ccb4c3a82177dc5726d5c360be11cb
Koichi Shiraishi <zchee.io@gmail.com>
2024-05-10 01:35:16 +0900

    telepresence-head: add telepresence-head formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/telepresence-head.rb
@@ -0,0 +1,19 @@
+class TelepresenceHead < Formula
+  desc "Local dev environment attached to a remote Kubernetes cluster"
+  homepage "https://telepresence.io"
+  head "https://github.com/telepresenceio/telepresence.git", branch: "release/v2"
+
+  depends_on "go" => :build
+  depends_on "jq" => :build
+
+  def install
+    system "make", "build-output/bin/telepresence"
+    bin.install "build-output/bin/telepresence"
+
+    generate_completions_from_executable(bin/"telepresence", "completion", base_name: "telepresence")
+  end
+
+  test do
+    system "#{bin}/telepresence", "--help"
+  end
+end
