01e4f150d74239c3b91c21e5794e34f8f3f6d61a
Koichi Shiraishi <zchee.io@gmail.com>
2024-08-01 12:52:02 +0900

    txtpbfmt: add grpcdebug formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/grpcdebug.rb
@@ -0,0 +1,27 @@
+class Grpcdebug < Formula
+  desc "grpcdebug is a command line interface focusing on simplifying the debugging process of gRPC applications."
+  homepage "https://github.com/grpc-ecosystem/grpcdebug"
+  license "Apache-2.0"
+  head "https://github.com/grpc-ecosystem/grpcdebug.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = %W[
+      -s -w
+    ]
+    tags = %W[
+      osusergo
+      netgo
+      static
+    ].join(",")
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "-tags=#{tags}"
+  end
+end
