b85a880d05778ec6082a330cf7f96c3cd61c0bbb
Koichi Shiraishi <zchee.io@gmail.com>
2024-08-19 19:23:30 +0900

    tmux-head: add jemalloc-head dependency
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/tmux-head.rb
+++ Formula/tmux-head.rb
@@ -1,8 +1,5 @@
 class TmuxHead < Formula
-  desc "Terminal multiplexer"
-  homepage "https://tmux.github.io/"
   license "ISC"
-  head "https://github.com/tmux/tmux.git", branch: "master"
 
   livecheck do
     url :stable
@@ -10,14 +7,21 @@ class TmuxHead < Formula
     strategy :github_latest
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
+  head do
+    url "https://github.com/tmux/tmux.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   depends_on "pkg-config" => :build
+  depends_on "jemalloc-head"
   depends_on "libevent-head"
   depends_on "ncurses-head"
 
-  uses_from_macos "bison" => :build # for yacc
+  depends_on "bison" => :build # for yacc
+  # uses_from_macos "bison" => :build # for yacc
 
   # Old versions of macOS libc disagree with utf8proc character widths.
   # https://github.com/tmux/tmux/issues/2223
@@ -25,33 +29,19 @@ class TmuxHead < Formula
     depends_on "utf8proc-head"
   end
 
-  # depends_on "autoconf" => :build
-  # depends_on "automake" => :build
-  # depends_on "libtool" => :build
-  # depends_on "pkg-config" => :build
-  #
-  # depends_on "bison" => :build
-  # depends_on "libevent-head" => :build
-  # depends_on "ncurses-head" => :build
-  # depends_on "pcre" => :build
-  # depends_on "utf8proc-head" => :build
-
   resource "completion" do
     url "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/f5d53239f7658f8e8fbaf02535cc369009c436d6/completions/tmux"
     sha256 "b5f7bbd78f9790026bbff16fc6e3fe4070d067f58f943e156bd1a8c3c99f6a6f"
   end
 
-  patch :DATA
-
   def install
     inreplace "configure.ac", /AC_INIT\(\[tmux\],[^)]*\)/, "AC_INIT([tmux], master)"
 
-    system "sh", "autogen.sh"
+    system "sh", "autogen.sh" if build.head?
 
     args = %W[
-      --disable-dependency-tracking
       --enable-sixel
-      --prefix=#{prefix}
+      --enable-jemalloc
       --sysconfdir=#{etc}
     ]
 
@@ -61,13 +51,11 @@ class TmuxHead < Formula
       # tools that link with the very old ncurses provided by macOS.
       # https://github.com/Homebrew/homebrew-core/issues/102748
       args << "--with-TERM=screen-256color" if MacOS.version < :sonoma
-      args << "--enable-utf8proc" #{Formula['utf8proc-head'].opt if MacOS.version >= :high_sierra
+      args << "--enable-utf8proc" if MacOS.version >= :high_sierra
       ENV["LIBUTF8PROC_CFLAGS"] = "-I#{Formula["utf8proc-head"].opt_include}" if MacOS.version >= :high_sierra
       ENV["LIBUTF8PROC_LIBS"] = "#{Formula["utf8proc-head"].opt_lib}/libutf8proc.a" if MacOS.version >= :high_sierra
     else
-      args << "--enable-utf8proc=" # "#{Formula['utf8proc-head'].opt"
-      ENV["LIBUTF8PROC_CFLAGS"] = "-I#{Formula["utf8proc-head"].opt_include}"
-      ENV["LIBUTF8PROC_LIBS"] = "#{Formula["utf8proc-head"].opt_lib}/libutf8proc.a"
+      args << "--enable-utf8proc"
     end
 
     ENV["LIBEVENT_CORE_CFLAGS"] = "-I#{Formula["libevent-head"].opt_include}"
@@ -77,15 +65,16 @@ class TmuxHead < Formula
     ENV["LIBNCURSES_CFLAGS"] = "-I#{Formula["ncurses-head"].opt_include} -I#{Formula["ncurses-head"].opt_include}/ncursesw"
     ENV["LIBNCURSES_LIBS"] = "#{Formula["ncurses-head"].opt_lib}/libncursesw.a"
     ENV["LIBNCURSESW_CFLAGS"] = "-I#{Formula["ncurses-head"].opt_include} -I#{Formula["ncurses-head"].opt_include}/ncursesw"
-    ENV["LIBNCURSESW_LIBS"] = "#{Formula["ncurses-head"].opt_lib}/libncursesw.a"
+    ENV["JEMALLOC_CFLAGS"] = "-I#{Formula["jemalloc-head"].opt_include}"
+    ENV["JEMALLOC_LIBS"] = "#{Formula["jemalloc-head"].opt_lib}/libjemalloc.a"
 
-    cflags = "-march=native -Ofast -flto -std=c2x -Wno-pointer-sign"
-    cflags += Hardware::CPU.intel? ? "-mcpu=x86-64-v4" : "-mcpu=apple-latest"
-    ldflags = "-march=native -Ofast -flto -L/usr/local/lib -lresolv"  # -lutil -lhoard?
+    cflags = "-march=native -Ofast -flto -std=c2x"
+    cflags += Hardware::CPU.intel? ? " -mcpu=x86-64-v4" : " -mcpu=apple-latest"
+    ldflags = "-march=native -Ofast -flto -L/usr/local/lib -lresolv"  # -lutil?
     ENV.append "CFLAGS", *cflags
     ENV.append "LDFLAGS", *ldflags
 
-    system "./configure", *args, *std_configure_args
+    system "./configure", *std_configure_args, *args
 
     system "make", "install"
 
