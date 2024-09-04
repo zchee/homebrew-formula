b082042d76a52c934b8fbd0d06121655ba829d7c
Koichi Shiraishi <zchee.io@gmail.com>
2024-09-05 00:14:53 +0900

    bottle-bomb: add bottle-bomb formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/bottle-bomb.rb
@@ -0,0 +1,27 @@
+class BottleBomb < Formula
+  desc "Homebrew Bottle Downloader"
+  homepage "https://github.com/blacktop/bottle-bomb"
+  license "MIT"
+  head "https://github.com/blacktop/bottle-bomb.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = %W[
+      -s -w
+    ]
+    tags = %W[
+      osusergo
+      netgo
+      static
+    ].join(",")
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "-tags=#{tags}"
+  end
+end
