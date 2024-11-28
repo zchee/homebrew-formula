5f44aa01a504cfa809c3354cd7f6364790725b04
Koichi Shiraishi <zchee.io@gmail.com>
2024-11-29 05:12:10 +0900

    ripgrep-head: fix rust_flags
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/ripgrep-head.rb
+++ Formula/ripgrep-head.rb
@@ -11,21 +11,43 @@ class RipgrepHead < Formula
 
   depends_on "asciidoctor" => :build
   depends_on "pkg-config" => :build
-  depends_on "pcre2" => :build
+  depends_on "pcre2"
 
   env :std
 
   def install
     root_dir = Hardware::CPU.intel? ? "/usr" : "/opt"
     target_cpu = Hardware::CPU.intel? ? "x86-64-v4" : "apple-latest"
-    features = %w(pcre2)
+    rust_flags = %W[
+      -C target-cpu=native
+      -C target-cpu=#{target_cpu}
+      -C opt-level=3
+      -C force-frame-pointers=on
+      -C debug-assertions=off
+      -C incremental=on
+      -C overflow-checks=off
+      -C panic=abort
+      -C codegen-units=1
+      -C embed-bitcode=yes
+      -C strip=symbols
+      -Z dylib-lto
+      -Z location-detail=none
+    ]
+    if Hardware::CPU.intel?
+      rust_flags << "-C target-feature=+aes,+avx,+avx2,+avx512f,+avx512dq,+avx512cd,+avx512bw,+avx512vl,+avx512vnni"
+    end
+
+    # setup nightly cargo with rustup
+    ENV.append_path "PATH", "#{root_dir}/local/rust/rustup/bin"
+    ENV["RUSTUP_HOME"] = "#{root_dir}/local/rust/rustup"
+    ENV["RUSTFLAGS"] = "#{rust_flags}"
 
     ENV.append_path "PATH", "#{ENV["HOMEBREW_PREFIX"]}/rust/rustup/bin"
     ENV["RUSTUP_HOME"] = "#{ENV["HOMEBREW_PREFIX"]}/rust/rustup"
     ENV["RUSTFLAGS"] = "-C target-cpu=native"
     ENV["PCRE2_SYS_STATIC"] = "1"
 
-    system "rustup", "run", "nightly", "cargo", "build", "--release", "--features", "#{features.join(" ")}"
+    system "rustup", "run", "nightly", "cargo", "install", "--features", "pcre2", *std_cargo_args
     bin.install "target/release/rg"
 
     generate_completions_from_executable(bin/"rg", "--generate", base_name: "rg", shell_parameter_format: "complete-")
@@ -34,6 +56,6 @@ class RipgrepHead < Formula
 
   test do
     (testpath/"Hello.txt").write("Hello World!")
-    system "#{bin}/rg", "Hello World!", testpath
+    system bin/"rg", "Hello World!", testpath
   end
 end
