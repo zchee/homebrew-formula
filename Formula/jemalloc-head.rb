a1b9b4fae40623b38635068f3dfba6515503977f
Koichi Shiraishi <zchee.io@gmail.com>
2025-07-25 06:46:24 +0900

    jemalloc-head: switch to facebook/jemalloc
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/jemalloc-head.rb
+++ Formula/jemalloc-head.rb
@@ -1,13 +1,16 @@
 class JemallocHead < Formula
   desc "Implementation of malloc emphasizing fragmentation avoidance"
-  homepage "http://jemalloc.net/"
+  homepage "https://github.com/facebook/jemalloc"
   license "BSD-2-Clause"
 
   head do
-    url "https://github.com/jemalloc/jemalloc.git", branch: "dev"
+    url "https://github.com/facebook/jemalloc.git", branch: "dev"
 
     depends_on "autoconf" => :build
     depends_on "docbook-xsl" => :build
+    depends_on "libxslt" => :build
+
+    patch :DATA
   end
 
   def install
@@ -15,6 +18,7 @@ class JemallocHead < Formula
       --disable-debug
       --prefix=#{prefix}
       --with-jemalloc-prefix=
+      --enable-experimental-fp-prefetch
     ]
 
     if build.head?
@@ -54,3 +58,65 @@ class JemallocHead < Formula
     system "./test"
   end
 end
+
+__END__
+diff --git a/configure.ac b/configure.ac
+index c703a6d10..1b655cef2 100644
+--- a/configure.ac
++++ b/configure.ac
+@@ -1434,6 +1434,22 @@ if test "x$enable_experimental_smallocx" = "x1" ; then
+ fi
+ AC_SUBST([enable_experimental_smallocx])
+ 
++dnl Do not enable fastpath prefetch by default.
++AC_ARG_ENABLE([experimental_fp_prefetch],
++  [AS_HELP_STRING([--enable-experimental-fp-prefetch], [Enable experimental fastpath prefetch])],
++[if test "x$enable_experimental_fp_prefetch" = "xno" ; then
++enable_experimental_fp_prefetch="0"
++else
++enable_experimental_fp_prefetch="1"
++fi
++],
++[enable_experimental_fp_prefetch="0"]
++)
++if test "x$enable_experimental_fp_prefetch" = "x1" ; then
++  AC_DEFINE([JEMALLOC_EXPERIMENTAL_FASTPATH_PREFETCH], [ ], [ ])
++fi
++AC_SUBST([enable_experimental_fp_prefetch])
++
+ dnl Do not enable profiling by default.
+ AC_ARG_ENABLE([prof],
+   [AS_HELP_STRING([--enable-prof], [Enable allocation profiling])],
+diff --git a/include/jemalloc/internal/jemalloc_internal_defs.h.in b/include/jemalloc/internal/jemalloc_internal_defs.h.in
+index 31ae2e8ed..3a945ba1e 100644
+--- a/include/jemalloc/internal/jemalloc_internal_defs.h.in
++++ b/include/jemalloc/internal/jemalloc_internal_defs.h.in
+@@ -160,6 +160,11 @@
+ /* JEMALLOC_EXPERIMENTAL_SMALLOCX_API enables experimental smallocx API. */
+ #undef JEMALLOC_EXPERIMENTAL_SMALLOCX_API
+ 
++/* JEMALLOC_EXPERIMENTAL_FASTPATH_PREFETCH enables prefetch
++ * on malloc fast path.
++ */
++#undef JEMALLOC_EXPERIMENTAL_FASTPATH_PREFETCH
++
+ /* JEMALLOC_PROF enables allocation profiling. */
+ #undef JEMALLOC_PROF
+ 
+diff --git a/include/jemalloc/internal/jemalloc_internal_inlines_c.h b/include/jemalloc/internal/jemalloc_internal_inlines_c.h
+index 2c61f8c4f..60f3b2b13 100644
+--- a/include/jemalloc/internal/jemalloc_internal_inlines_c.h
++++ b/include/jemalloc/internal/jemalloc_internal_inlines_c.h
+@@ -374,6 +374,12 @@ imalloc_fastpath(size_t size, void *(fallback_alloc)(size_t)) {
+ 	 */
+ 	ret = cache_bin_alloc_easy(bin, &tcache_success);
+ 	if (tcache_success) {
++#if __GNUC__ && defined(JEMALLOC_EXPERIMENTAL_FASTPATH_PREFETCH)
++		cache_bin_sz_t lb = (cache_bin_sz_t)(uintptr_t)bin->stack_head;
++		if(likely(lb != bin->low_bits_empty)) {
++			util_prefetch_write_range(*(bin->stack_head), usize);
++		}
++#endif
+ 		fastpath_success_finish(tsd, allocated_after, bin, ret);
+ 		return ret;
+ 	}
