36d5d0d1b61b30c743d83df0575b0645d62cae81
Koichi Shiraishi <zchee.io@gmail.com>
2025-07-26 01:47:02 +0900

    slack-cli-head: fix revision
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/slack-cli-head.rb
+++ Formula/slack-cli-head.rb
@@ -2,7 +2,6 @@ class SlackCliHead < Formula
   desc "Create, develop, and deploy Slack apps from the command-line"
   homepage "https://github.com/slackapi/slack-cli"
   license "Apache-2.0"
-
   head "https://github.com/slackapi/slack-cli.git", branch: "main"
 
   livecheck do
@@ -13,8 +12,11 @@ class SlackCliHead < Formula
   depends_on "go" => :build
 
   def install
-    ldflags_version = Utils.safe_popen_read("git describe --abbrev=0 --tags").chomp
-    ldflags = "-s -w -X github.com/slackapi/slack-cli/internal/pkg/version.Version=#{ldflags_version}"
+    ldflags_version = Utils.safe_popen_read("git rev-parse --short=12 --verify HEAD").chomp
+    ldflags = %W[
+      -s -w
+      -X github.com/slackapi/slack-cli/internal/pkg/version.Version=#{ldflags_version}
+    ]
     system "go", "build", *std_go_args(output: bin/"slack", ldflags:), "."
 
     generate_completions_from_executable(bin/"slack", "completion", shells: [:bash, :zsh, :fish, :pwsh], base_name: "slack")
