8a7b4eca3a121244de11dc682123b205cc11999e
Koichi Shiraishi <zchee.io@gmail.com>
2025-07-26 04:36:01 +0900

    bazelisk-head: add bazelisk-head formula
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ Formula/bazelisk-head.rb
@@ -0,0 +1,39 @@
+class BazeliskHead < Formula
+  desc "User-friendly launcher for Bazel"
+  homepage "https://github.com/bazelbuild/bazelisk/"
+  license "Apache-2.0"
+  head "https://github.com/bazelbuild/bazelisk.git", branch: "master"
+
+  depends_on "go" => :build
+
+  conflicts_with "bazel", because: "Bazelisk replaces the bazel binary"
+
+  resource "bazel_zsh_completion" do
+    url "https://raw.githubusercontent.com/bazelbuild/bazel/036e5337f63d967bb4f5fea78dc928d16d0b213c/scripts/zsh_completion/_bazel"
+    sha256 "4094dc84add2f23823bc341186adf6b8487fbd5d4164bd52d98891c41511eba4"
+  end
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/bazelbuild/bazelisk/core.BazeliskVersion=#{version}")
+
+    bin.install_symlink "bazelisk" => "bazel"
+
+    resource("bazel_zsh_completion").stage do
+      zsh_completion.install "_bazel"
+    end
+  end
+
+  test do
+    ENV["USE_BAZEL_VERSION"] = Formula["bazel"].version
+    output = shell_output("#{bin}/bazelisk version")
+    assert_match "Bazelisk version: #{version}", output
+    assert_match "Build label: #{Formula["bazel"].version}", output
+
+    # This is an older than current version, so that we can test that bazelisk
+    # will target an explicit version we specify. This version shouldn't need to
+    # be bumped.
+    bazel_version = Hardware::CPU.arm? ? "7.1.0" : "7.0.0"
+    ENV["USE_BAZEL_VERSION"] = bazel_version
+    assert_match "Build label: #{bazel_version}", shell_output("#{bin}/bazelisk version")
+  end
+end
