70a2e8e56f5faea9c13bd4c3514854bffd31e2cc
Koichi Shiraishi <zchee.io@gmail.com>
2025-08-27 15:40:10 +0900

    hyperfine-head: support darwin/arm64
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/hyperfine-head.rb
+++ Formula/hyperfine-head.rb
@@ -1,7 +1,6 @@
 class HyperfineHead < Formula
   desc "Command-line benchmarking tool"
   homepage "https://github.com/sharkdp/hyperfine"
-  head "https://github.com/sharkdp/hyperfine.git", branch: "master"
   license any_of: ["Apache-2.0", "MIT"]
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/sharkdp/hyperfine.git", branch: "master"
@@ -9,22 +8,22 @@ class HyperfineHead < Formula
   env :std
 
   def install
-    # setup nightly cargo with rustup
-    ENV.append_path "PATH", "/usr/local/rust/rustup/bin"
-    ENV["RUSTUP_HOME"] = "/usr/local/rust/rustup"
-    ENV["RUSTFLAGS"] = "-C target-cpu=native -C target-cpu=x86-64-v4 -C target-feature=+aes,+avx,+avx2,+avx512f,+avx512dq,+avx512cd,+avx512bw,+avx512vl"
+    root_dir = Hardware::CPU.intel? ? "/usr" : "/opt"
+    target_cpu = Hardware::CPU.intel? ? "x86-64-v4" : "apple-latest"
 
-    # avoid invalid data in index - calculated checksum does not match expected
-    system "git", "config", "--local", "index.skipHash", "false"
+    # setup nightly cargo with rustup
+    ENV.append_path "PATH", "#{root_dir}/local/rust/rustup/bin"
+    ENV["RUSTUP_HOME"] = "#{root_dir}/local/rust/rustup"
+    ENV["RUSTFLAGS"] = "-C target-cpu=native -C target-cpu=#{target_cpu}"
 
     ENV["SHELL_COMPLETIONS_DIR"] = buildpath
 
     system "rustup", "run", "nightly", "cargo", "install", "--all-features", "--root", prefix, "--path", "."
 
-    man1.install "doc/hyperfine.1"
-    bash_completion.install "hyperfine.bash"
+    bash_completion.install "hyperfine.bash" => "hyperfine"
     fish_completion.install "hyperfine.fish"
     zsh_completion.install "_hyperfine"
+    man1.install "doc/hyperfine.1"
   end
 
   test do
