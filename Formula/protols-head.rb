ec0ff2ae640d703347c9d39275345ec664aa84d5
Koichi Shiraishi <zchee.io@gmail.com>
2026-02-18 05:46:34 +0900

    protols-head: fix rustup setup
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- Formula/protols-head.rb
+++ Formula/protols-head.rb
@@ -7,19 +7,19 @@ class ProtolsHead < Formula
   env :std
 
   def install
+    # setup cargo with rustup
     root_dir = Hardware::CPU.intel? ? "/usr" : "/opt"
     target_cpu = Hardware::CPU.intel? ? "x86-64-v4" : "apple-latest"
-
-    # setup nightly cargo with rustup
     ENV.append_path "PATH", "#{root_dir}/local/rust/rustup/bin"
     ENV["RUSTUP_HOME"] = "#{root_dir}/local/rust/rustup"
-    ENV["RUSTFLAGS"] = "-C target-cpu=native -C target-cpu=#{target_cpu}"
+    ENV["RUSTFLAGS"] = "-C target-cpu=native -C target-cpu=#{target_cpu} -C opt-level=3 -C force-frame-pointers=on -C debug-assertions=off -C incremental=on -C overflow-checks=off"
 
-    inreplace "rust-toolchain.toml", 'channel = "stable"', 'channel = "nightly"'
+    # setup sccache
+    sccache_dir = "#{Etc.getpwuid.dir}/.cache/sccache"
+    mkdir_p sccache_dir
+    ENV["RUSTC_WRAPPER"] = "#{Formula["sccache"].opt_bin}/sccache"
+    ENV["SCCACHE_DIR"] = sccache_dir
 
-    system "rustup", "run", "nightly", "cargo", "install", "--all-features", "--root", prefix, "--path", "."
-
-    # generate_completions_from_executable(bin/"fd", "--gen-completions", shells: [:bash, :fish], base_name: "fd")
-    # zsh_completion.install "contrib/completion/_fd"
+    system "cargo", "install", "--verbose", "--all-features", "--root", prefix, "--path", "."
   end
 end
