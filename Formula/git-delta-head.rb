b7943a635514eef6e670b85261db209b954c3e47
Koichi Shiraishi <zchee.io@gmail.com>
2026-03-11 11:52:52 +0900

    git-delta-head: use std rustup

--- Formula/git-delta-head.rb
+++ Formula/git-delta-head.rb
@@ -4,12 +4,25 @@ class GitDeltaHead < Formula
   license "MIT"
   head "https://github.com/dandavison/delta.git", :branch => "main"
 
-  depends_on "rust" => :build
-  depends_on "llvm" => :build
+  env :std
+
   depends_on "zlib" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args, "--all-features"
+    # setup cargo with rustup
+    root_dir = Hardware::CPU.intel? ? "/usr" : "/opt"
+    target_cpu = Hardware::CPU.intel? ? "x86-64-v4" : "apple-latest"
+    ENV.append_path "PATH", "#{root_dir}/local/rust/rustup/bin"
+    ENV["RUSTUP_HOME"] = "#{root_dir}/local/rust/rustup"
+    ENV["RUSTFLAGS"] = "-C target-cpu=native -C target-cpu=#{target_cpu} -C opt-level=3 -C force-frame-pointers=on -C debug-assertions=off -C incremental=on -C overflow-checks=off"
+
+    # setup sccache
+    sccache_dir = "#{Etc.getpwuid.dir}/.cache/sccache"
+    mkdir_p sccache_dir
+    ENV["RUSTC_WRAPPER"] = "#{Formula["sccache"].opt_bin}/sccache"
+    ENV["SCCACHE_DIR"] = sccache_dir
+
+    system "cargo", "install", *std_cargo_args, "--verbose", "--all-features"
     bin.install_symlink "delta" => "git-delta"
 
     zsh_completion.install "etc/completion/completion.zsh" => "_delta"
