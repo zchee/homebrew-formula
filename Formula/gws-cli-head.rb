0bf32e5f55ddd607adb641201e9841ace5e003fb
Koichi Shiraishi <zchee.io@gmail.com>
2026-03-11 18:50:20 +0900

    gws-cli-head: add gws-cli-head formula

---
+++ Formula/gws-cli-head.rb
@@ -0,0 +1,27 @@
+class GwsCliHead < Formula
+  desc "Google Workspace CLI"
+  homepage "https://github.com/googleworkspace/cli"
+  license "Apache-2.0"
+  head "https://github.com/googleworkspace/cli.git", branch: "main"
+
+  env :std
+
+  depends_on "sccache" => :build
+
+  def install
+    # setup cargo with rustup
+    root_dir = Hardware::CPU.intel? ? "/usr" : "/opt"
+    target_cpu = Hardware::CPU.intel? ? "x86-64-v4" : "apple-latest"
+    ENV.append_path "PATH", "#{root_dir}/local/rust/rustup/bin"
+    ENV["RUSTUP_HOME"] = "#{root_dir}/local/rust/rustup"
+    ENV["RUSTFLAGS"] = "-C target-cpu=native -C target-cpu=#{target_cpu} -C opt-level=3 -C force-frame-pointers=on -C debug-assertions=off -C incremental=on -C overflow-checks=off"
+
+    # setup sccache
+    sccache_dir = "#{Etc.getpwuid.dir}/.cache/sccache"
+    mkdir_p sccache_dir
+    ENV["RUSTC_WRAPPER"] = "#{Formula["sccache"].opt_bin}/sccache"
+    ENV["SCCACHE_DIR"] = sccache_dir
+
+    system "cargo", "install", "--verbose", "--all-features", *std_cargo_args
+  end
+end
