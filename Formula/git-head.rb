90fe2ff6f254a1b11fd014fd53b44cecc6e17377
Koichi Shiraishi <zchee.io@gmail.com>
2026-03-11 18:50:02 +0900

    git-head: fix curl-cloudflare depends_on to curl

--- Formula/git-head.rb
+++ Formula/git-head.rb
@@ -12,7 +12,7 @@ class GitHead < Formula
   depends_on "asciidoc"
   depends_on "c-ares"
 
-  depends_on "curl-cloudflare"
+  depends_on "curl"
   depends_on "brotli"
   depends_on "libidn2"
   depends_on "libnghttp2"
