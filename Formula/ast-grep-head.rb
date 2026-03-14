4c3231832953a88714fcf97ffbbb62b3ce7b3b27
Koichi Shiraishi <zchee.io@gmail.com>
2026-03-14 11:49:35 +0900

    ast-grep-head: add ast-grep-head formula

---
+++ Formula/ast-grep-head.rb
@@ -0,0 +1,35 @@
+class AstGrepHead < Formula
+  desc "Code searching, linting, rewriting"
+  homepage "https://github.com/ast-grep/ast-grep"
+  license "MIT"
+  head "https://github.com/ast-grep/ast-grep.git", branch: "main"
+
+  env :std
+
+  def install
+    # setup cargo with rustup
+    root_dir = Hardware::CPU.intel? ? "/usr" : "/opt"
+    target_cpu = Hardware::CPU.intel? ? "x86-64-v4" : "apple-latest"
+    ENV.append_path "PATH", "#{root_dir}/local/rust/rustup/bin"
+    ENV["RUSTUP_HOME"] = "#{root_dir}/local/rust/rustup"
+    ENV["RUSTFLAGS"] = "-C target-cpu=native -C target-cpu=#{target_cpu} -C opt-level=3 -C force-frame-pointers=on -C debug-assertions=off -C incremental=on -C overflow-checks=off"
+
+    # setup sccache
+    sccache_dir = "#{Etc.getpwuid.dir}/.cache/sccache"
+    mkdir_p sccache_dir
+    ENV["RUSTC_WRAPPER"] = "#{Formula["sccache"].opt_bin}/sccache"
+    ENV["SCCACHE_DIR"] = sccache_dir
+
+    system "cargo", "install", "--verbose", "--all-features", *std_cargo_args(path: "crates/cli")
+
+    generate_completions_from_executable(bin/"ast-grep", "completions")
+  end
+
+  test do
+    (testpath/"hi.js").write("console.log('it is me')")
+    system bin/"ast-grep", "run", "-l", "js", "-p console.log", (testpath/"hi.js
+")
+
+    assert_match version.to_s, shell_output("#{bin}/ast-grep --version")
+  end
+end
