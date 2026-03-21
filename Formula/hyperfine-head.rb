3ddddb408c3457053b94eeb10dd8e9542f0a8abf
Koichi Shiraishi <zchee.io@gmail.com>
2026-03-22 02:37:56 +0900

    hyperfine-head: use stable cargo

--- Formula/hyperfine-head.rb
+++ Formula/hyperfine-head.rb
@@ -23,7 +23,7 @@ class HyperfineHead < Formula
 
     ENV["SHELL_COMPLETIONS_DIR"] = buildpath
 
-    system "cargo", "install", "--all-features", "--root", prefix, "--path", "."
+    system "rustup", "run", "stable", "cargo", "install", "--all-features", "--root", prefix, "--path", "."
 
     bash_completion.install "hyperfine.bash" => "hyperfine"
     fish_completion.install "hyperfine.fish"
