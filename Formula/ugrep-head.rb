5b56ac2800cd77d7910e3933ba65144650c0bca3
Koichi Shiraishi <zchee.io@gmail.com>
2026-07-14 07:19:02 +0900

    ugrep-head: fix styling

--- Formula/ugrep-head.rb
+++ Formula/ugrep-head.rb
@@ -6,7 +6,6 @@ class UgrepHead < Formula
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "libtool" => :head
   depends_on "boost" => :build
   depends_on "brotli" => :build
   depends_on "bzip2" => :build
@@ -16,6 +15,7 @@ class UgrepHead < Formula
   depends_on "xz" => :build
   depends_on "zlib" => :build
   depends_on "zstd" => :build
+  depends_on "libtool" => :head
 
   def install
     system "autoreconf", "-fiv"
@@ -28,33 +28,43 @@ class UgrepHead < Formula
       --disable-dependency-tracking
       --disable-silent-rules
       --prefix=#{prefix}
-      --with-boost-regex=#{Formula["boost"].opt_prefix}
-      --with-brotli=#{Formula["brotli"].opt_prefix}
-      --with-bzlib=#{Formula["bzip2"].opt_prefix}
-      --with-bzip3=#{Formula["bzip3"].opt_prefix}
-      --with-lz4=#{Formula["lz4"].opt_prefix}
-      --with-pcre2=#{Formula["pcre2"].opt_prefix}
-      --with-lzma=#{Formula["xz"].opt_prefix}
-      --with-zlib=#{Formula["zlib"].opt_prefix}
-      --with-zstd=#{Formula["zstd"].opt_prefix}
+      --with-boost-regex=#{formula_opt_prefix("boost")}
+      --with-brotli=#{formula_opt_prefix("brotli")}
+      --with-bzlib=#{formula_opt_prefix("bzip2")}
+      --with-bzip3=#{formula_opt_prefix("bzip3")}
+      --with-lz4=#{formula_opt_prefix("lz4")}
+      --with-pcre2=#{formula_opt_prefix("pcre2")}
+      --with-lzma=#{formula_opt_prefix("xz")}
+      --with-zlib=#{formula_opt_prefix("zlib")}
+      --with-zstd=#{formula_opt_prefix("zstd")}
       --with-bash-completion-dir
       --with-fish-completion-dir
       --with-zsh-completion-dir
     ]
 
-    target_cpu = Hardware::CPU.intel? ? "x86-64-v4" : %x( sysctl -n machdep.cpu.brand_string | awk '{ print tolower($1"-"$2) }' )
-    cxxflags = "-march=native -mcpu=#{target_cpu} -O3 -flto -std=c++20"
+    if Hardware::CPU.intel?
+      cflags = "-march=x86-64-v4 -O3 -funroll-loops -ffast-math -fforce-addr -flto -std=c2x"
+      cxxflags  = "-march=x86-64-v4 -O3 -funroll-loops -ffast-math -fforce-addr -flto -std=c++20"
+      ldflags = "-march=x86-64-v4 -O3 -funroll-loops -ffast-math -fforce-addr -flto"
+    else
+      cpu = `sysctl -n machdep.cpu.brand_string | awk '{ print tolower($1"-"$2) }'`.chomp
+      cflags = "-mcpu=#{cpu} -O3 -funroll-loops -ffast-math -fforce-addr -flto -std=c2x"
+      cxxflags  = "-mcpu=#{cpu} -O3 -funroll-loops -ffast-math -fforce-addr -flto -std=c++20"
+      ldflags = "-mcpu=#{cpu} -O3 -funroll-loops -ffast-math -fforce-addr -flto"
+    end
     libs = %W[
-      #{Formula["pcre2"].opt_lib}/libpcre2-8.a
-      #{Formula["zlib"].opt_lib}/libz.a
-      #{Formula["bzip2"].opt_lib}/libbz2.a
-      #{Formula["xz"].opt_lib}/liblzma.a
-      #{Formula["lz4"].opt_lib}/liblz4.a
-      #{Formula["zstd"].opt_lib}/libzstd.a
-      #{Formula["bzip3"].opt_lib}/libbzip3.a
+      #{formula_opt_lib("bzip2")}/libbz2.a
+      #{formula_opt_lib("bzip3")}/libbzip3.a
+      #{formula_opt_lib("lz4")}/liblz4.a
+      #{formula_opt_lib("pcre2")}/libpcre2-8.a
+      #{formula_opt_lib("xz")}/liblzma.a
+      #{formula_opt_lib("zlib")}/libz.a
+      #{formula_opt_lib("zstd")}/libzstd.a
     ]
 
+    ENV.append "CFLAGS", *cflags
     ENV.append "CXXFLAGS", *cxxflags
+    ENV.append "LDFLAGS", *ldflags
     ENV.append "LIBS", libs.join(" ")
 
     system "./configure", *args
